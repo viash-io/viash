8f42752b1399a02f87b2d8b7a8e901c5a084ebce
Robrecht Cannoodt <rcannood@gmail.com>
2020-03-12 11:58:44 +0100

    wip poc

--- src/main/scala/com/dataintuitive/viash/Main.scala
+++ src/main/scala/com/dataintuitive/viash/Main.scala
@@ -1,45 +1,84 @@
 package com.dataintuitive.viash
 
-import org.rogach.scallop.ScallopConf
-
+import org.rogach.scallop.{ScallopConf, Subcommand}
 import scala.io.Source
-
 import io.circe.yaml.parser
-import io.circe.yaml.syntax._
-
-import cats.syntax.either._
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.syntax._
-
 import functionality._
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val mode = trailArg[String](default = Some("test"))
-  val pSpec = opt[String](default = Some("portash.yaml"), descr = "Portash spec (default portash.yaml)")
-  val vSpec = opt[String](default = Some("viash.yaml"), descr = "Viash spec (default viash.yaml)")
+  val functionality = opt[String](
+    default = Some("functionality.yaml"), 
+    descr = "Path to the functionality specifications (default functionality.yaml)",
+    required = true
+  )
+  val platform = opt[String](
+    default = Some("platform.yaml"), 
+    descr = "Path to the platform specifications (default platform.yaml)",
+    required = true
+  )
+  
+  val run = new Subcommand("run") {
+    val test = opt[String](
+      descr = "Test argument"
+    )
+  }
+  val export = new Subcommand("export") {
+    val output = opt[String](
+      default = Some("executable.sh"), 
+      descr = "Path to output executable.",
+      required = true
+    )
+  }
+  
+  addSubcommand(run)
+  addSubcommand(export)
+  
   verify()
 }
 
 
+
+
 object Main {
+  def parseFunctionality(filename: String): Functionality = {
+    val str = Source.fromFile(filename).mkString
+    val json = parser.parse(str)
+    val fun = json match {
+      case Right(js) => js.as[Functionality]
+      case Left(e) => throw e
+    }
+    fun match {
+      case Right(f) => f
+      case Left(e) => throw e
+    }
+  }
+  
+  def parsePlatform(filename: String): Unit = {
+//    val str = Source.fromFile(filename).mkString
+//    val json = parser.parse(str)
+//    val fun = json match {
+//      case Right(js) => js.as[Functionality]
+//      case Left(e) => throw e
+//    }
+//    fun match {
+//      case Right(f) => f
+//      case Left(e) => throw e
+//    }
+  }
+    
   def main(args: Array[String]) {
     val conf = new Conf(args)
-    println(s"mode = ${conf.mode()}, pSpec = ${conf.pSpec()}, vSpec = ${conf.vSpec()}")
-    println()
+    
+    println(s"functionality = ${conf.functionality()}, platform = ${conf.platform()}")
+    println(conf.subcommand)
+    println(conf.export.verified)
+    println(conf.run.verified)
+    println(s"export.output = ${conf.export.output()}")
+    println(s"run.test = ${conf.run.test}")
 
-    val pSpecStr = Source.fromFile(conf.pSpec()).mkString
-    val vSpecStr = Source.fromFile(conf.vSpec()).mkString
+    val functionality = parseFunctionality(conf.functionality())
+    val platform = parsePlatform(conf.platform())
     
-    val Right(json) = parser.parse(pSpecStr)
     
-    val Right(fun) = json.as[Functionality]
-    
-    import io.circe.yaml._
-
-    println(
-      Printer(dropNullKeys = true, mappingStyle = Printer.FlowStyle.Block)
-        .pretty(json)
-    )
   }
 }
--- src/main/scala/com/dataintuitive/viash/functionality/Resource.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/Resource.scala
@@ -1,12 +1,21 @@
 package com.dataintuitive.viash.functionality
 
+import java.io.File
+
 case class Resource(
   name: String,
-  path: Option[String],
+  path: Option[File],
   code: Option[String]
 ) {
   require(
     (path == None) != (code == None), 
-    "Exactly one of path and code must be defined, the other undefined."
+    message = "Exactly one of path and code must be defined, the other undefined."
   )
+  
+  if (path.isDefined) {
+    require(
+      path.get.exists(),
+      message = s"File at location '${path.get.getPath()}' does not exist."
+    )
+  }
 }
