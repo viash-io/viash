b6824605b31593491200e6ecb53679ac2811ab53
Robrecht Cannoodt <rcannood@gmail.com>
2020-03-25 14:27:01 +0100

    add dummy python example

---
+++ atoms/print_params/code.py
@@ -0,0 +1,14 @@
+
+### PORTASH START
+par = {
+  'whole_number': 1001,
+  'real_number': 123.987654,
+  'sentence': '...',
+  'truth': True,
+  'output': 'output.txt'
+}
+### PORTASH END
+
+for key in par.keys():
+	print(key, par[key])
+
