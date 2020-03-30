a7c984e0c249f6647b60a54a53b3a81b64292847
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-03-30 14:21:05 +0200

    Assemble once (sbt assembly) and run n times

--- temp/script1.sh
+++ temp/script1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sbt "run -f atoms/print_params/functionality.yaml -p atoms/print_params/platform_docker.yaml export -o output/print_params_docker"
-sbt "run -f atoms/print_params/functionality.yaml -p atoms/print_params/platform_native.yaml export -o output/print_params_native"
-sbt "run -f atoms/filter/functionality.yaml -p atoms/filter/platform_docker.yaml export -o output/filter_docker"
-sbt "run -f atoms/filter/functionality.yaml -p atoms/filter/platform_native.yaml export -o output/filter_native"
+sbt assembly
+
+JAVA="java -jar target/scala-2.12/viash-assembly-0.0.1.jar"
+
+$JAVA -f atoms/print_params/functionality.yaml -p atoms/print_params/platform_docker.yaml export -o output/print_params_docker
+
+$JAVA -f atoms/print_params/functionality.yaml -p atoms/print_params/platform_native.yaml export -o output/print_params_native
+
+$JAVA -f atoms/filter/functionality.yaml -p atoms/filter/platform_docker.yaml export -o output/filter_docker
+
+$JAVA -f atoms/filter/functionality.yaml -p atoms/filter/platform_native.yaml export -o output/filter_native
+
+$JAVA -f atoms/binary/functionality.yaml -p atoms/binary/platform_docker.yaml export -o output/binary_docker
