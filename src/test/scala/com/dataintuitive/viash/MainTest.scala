63c2769921b9640dde60625ce7f92a1fe67670a4
Robrecht Cannoodt <rcannood@gmail.com>
2020-04-14 21:18:14 +0200

    initial code for unit testing viash

--- src/test/scala/com/dataintuitive/viash/MainTest.scala
+++ src/test/scala/com/dataintuitive/viash/MainTest.scala
@@ -1,16 +1,34 @@
 package com.dataintuitive.viash
 
 import org.scalatest.FunSuite
+import java.nio.file.{Path, Paths}
 
 class MainTest extends FunSuite {
+  
+  def createTempDir(tmpName: String): String = {
+  	val tmpDir = Paths.get(System.getProperty("java.io.tmpdir"))
+  	val name: Path = tmpDir.getFileSystem.getPath(tmpName)
+  	if (name.getParent != null) throw new IllegalArgumentException("Invalid prefix or suffix")
+  	tmpDir.resolve(name).toString
+  }
+  
+  val temporaryFolder = createTempDir("output_testpython")
 
-  test("An empty Set should have size 0") {
-    assert(Set.empty.size == 0)
+  val params = Array(
+      "-f", getClass.getResource("/testpython/functionality.yaml").getPath, 
+      "-p", getClass.getResource("/testpython/platform_native.yaml").getPath, 
+      "export",
+      "-o", temporaryFolder
+     )
+  //println(params.mkString(" "))
+  Main.main(params)
+  
+  
+  val testpython = Paths.get(temporaryFolder, "testpython").toFile()
+
+  test("Viash should have created an executable named testpython") {
+    assert(testpython.exists())
+    assert(testpython.canExecute())
   }
 
-  test("Invoking head on an empty Set should produce NoSuchElementException") {
-    assertThrows[NoSuchElementException] {
-      Set.empty.head
-    }
-  }
 }
