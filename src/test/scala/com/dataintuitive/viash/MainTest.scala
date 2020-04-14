dbe298412e1aa1bdd5d8dee2584bb490c3ddb780
Robrecht Cannoodt <rcannood@gmail.com>
2020-04-14 11:44:33 +0200

    try testing on travis

---
+++ src/test/scala/com/dataintuitive/viash/MainTest.scala
@@ -0,0 +1,16 @@
+package com.dataintuitive.viash
+
+import org.scalatest.FunSuite
+
+class MainTest extends FunSuite {
+
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size == 0)
+  }
+
+  test("Invoking head on an empty Set should produce NoSuchElementException") {
+    assertThrows[NoSuchElementException] {
+      Set.empty.head
+    }
+  }
+}
