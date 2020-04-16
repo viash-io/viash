bf041cc941b6edee2f0799d09b019f3fb51014db
Robrecht Cannoodt <rcannood@gmail.com>
2020-04-16 22:08:46 +0200

    fix docker + bash

--- src/test/resources/testbash/code.sh
+++ src/test/resources/testbash/code.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
 # PORTASH START
-declare -A PAR
-PAR["abc"]=1
-PAR["in"]="testbash"
+declare -A par
+par_input="input.sh"
+par_real_number="123.987654"
+par_whole_number="17"
+par_s="test string"
+par_truth="true"
+par_output="output.txt"
+par_log="log.txt"
+par_optional="help"
+par_optional_with_default="me"
 # PORTASH END
 
-echo "## This is your abc parameter: ${PAR["abc"]}"
-echo "## Now reading from file '${PAR["in"]}'"
+function log {
+  if [ -z "$par_log" ]; then
+    echo $@
+  else
+    echo $@ > $par_log
+  fi
+}
 
-cat "${PAR["in"]}"
+log "INFO: Parsed input arguments."
+
+if [ -z "$par_output" ]; then
+  log "INFO: Printing output to console"
+  typeset -p | grep par_ | sed 's#"$##' | sed 's#.*par_\([^=]*\)="*\(.*\)$#\1: "\2"#'
+else
+  log "INFO: Writing output to file"
+  typeset -p | grep par_ | sed 's#"$##' | sed 's#.*par_\([^=]*\)="*\(.*\)$#"\1": "\2",#' | tr '\n' ' ' | sed 's#\(.*\), #{\1}\n#' > $par_output
+fi
+
+
