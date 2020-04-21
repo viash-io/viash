5b8454906def09e8aa7cfd0622111bb9ac875e26
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-04-21 18:13:41 +0200

    Rudimentary setup for some e2e test functionality

--- temp/test.sh
+++ temp/test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-sbt 'set test in assembly := {}' clean assembly
+echo "Fetch the test.txt file from in/ and store the filtered version in out/"
 
-JAVA="java -jar target/scala-2.12/viash-assembly-0.0.1.jar"
-TEST=src/test/resources
+echo "...run"
+output/wrapped_filter_docker/wrapped_filter \
+  --in "$PWD/in" \
+  --out "$PWD/out" \
+  --input "/in/test.txt" \
+  --output "/out/output.txt" \
+  --filter "echo" \
+  -b
 
-$JAVA -f $TEST/testpython/functionality.yaml -p $TEST/testpython/platform_docker.yaml export -o output/testpython_docker
-$JAVA -f $TEST/testpython/functionality.yaml -p $TEST/testpython/platform_native.yaml export -o output/testpython_native
-$JAVA -f $TEST/testbash/functionality.yaml -p $TEST/testbash/platform_docker.yaml export -o output/testbash_docker
-$JAVA -f $TEST/testbash/functionality.yaml -p $TEST/testbash/platform_native.yaml export -o output/testbash_native
+echo "test..."
+result=`grep "echo" out/output.txt`
+if [[ $? == 0 ]]; then
+  echo "OK"
+else
+  echo "NOK"
+fi
 
