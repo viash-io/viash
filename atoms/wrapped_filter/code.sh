6c435cebfcba331877b52af4648b42b5ae35ec59
Robrecht Cannoodt <rcannood@gmail.com>
2020-04-21 22:58:32 +0200

    add support for flagValues, positional arguments, and passthrough parameters

--- atoms/wrapped_filter/code.sh
+++ atoms/wrapped_filter/code.sh
@@ -4,12 +4,12 @@
 par_input="input.txt"
 par_output="output.txt"
 par_filter="AString"
-par_b="-b"
+PASSTHROUGH="-b" # passthrough
 # PORTASH END
 
 echo "## Reading from $par_input"
 echo "## Filtering for $par_filter"
 echo "## Writing to $par_output"
 echo "## Additional parameters? $CMDARGS"
-echo ">> Running cat $CMDARGS $par_b $par_input | grep $par_filter > $par_output"
-cat $CMDARGS $par_b "$par_input" | grep "$par_filter" > "$par_output"
+echo ">> Running cat $CMDARGS $par_input | grep $par_filter > $par_output"
+cat $PASSTHROUGH "$par_input" | grep "$par_filter" > "$par_output"
