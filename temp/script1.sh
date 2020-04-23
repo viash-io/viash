9036e5adbe80085fa361b01c18b9a3ca399e2fc1
Robrecht Cannoodt <rcannood@gmail.com>
2020-04-23 21:05:57 +0200

    rework scripts

--- temp/script1.sh
+++ temp/script1.sh
@@ -1,39 +1,18 @@
 #!/bin/bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-blue=`tput setaf 4`
-reset=`tput sgr0`
+source temp/helper.sh
 
-# sbt assembly
+# run_tests
+assembly
+# assembly_without_testing
 
-# sbt assembly
+viash_export_all atoms/ output/
+# viash_export_all src/test/resources/
 
-platforms=("native" "nextflow" "docker")
-
-JAVA="java -jar target/scala-2.12/viash-assembly-0.0.1.jar"
-
-for a in `ls atoms`; do
-  echo ">> ${green}Processing atom $a${reset}"
-  func_file="atoms/$a/functionality.yaml"
-  for p in ${platforms[@]}; do
-    echo ">>>> ${blue}Processing platform $p${reset}"
-    platform_file="atoms/$a/platform_$p.yaml"
-    if [ -f $platform_file ]; then
-      # echo "      $func_file"
-      # echo "      $platform_file"
-      # echo "$JAVA -f $func_file -p $platform_file export -o output/${a}_${p}"
-      $JAVA -f $func_file \
-            -p $platform_file \
-            export \
-            -o "output/${a}_${p}"
-    fi
-  done
-done
 
 # Some custom runs (NXF related)
-
-$JAVA -f atoms/wrapped_filter/functionality.yaml \
+viash \
+      -f atoms/wrapped_filter/functionality.yaml \
       -p atoms/wrapped_filter/platform_native.yaml \
       export \
       -o output/wrapped_filter_nextflow/bin
