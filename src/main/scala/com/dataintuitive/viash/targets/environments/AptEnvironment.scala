d1833f0259b4b9cfe4e45d6fce15ed6b061a9b78
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-04 20:57:51 +0200

    Fix issue #2, include issue in end-to-end test

--- src/main/scala/com/dataintuitive/viash/targets/environments/AptEnvironment.scala
+++ src/main/scala/com/dataintuitive/viash/targets/environments/AptEnvironment.scala
@@ -4,20 +4,20 @@ case class AptEnvironment(
   packages: Option[List[String]] = None
 ) {
   def getInstallCommands() = {
-    val aptUpdate = 
-      """apt-get update && """
-      
-    val installPackages = 
+    val aptUpdate =
+      """apt-get update"""
+
+    val installPackages =
       packages.getOrElse(Nil) match {
         case Nil => Nil
-        case packs => 
+        case packs =>
           List(packs.mkString(
-            "apt-get install -y ", 
-            " ", 
+            "apt-get install -y ",
+            " ",
             ""
           ))
       }
-      
+
     aptUpdate :: installPackages
   }
 }
