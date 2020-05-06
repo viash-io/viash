abdc8543b1a52088fafed3b564b3196145ba6127
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-06 22:40:39 +0200

    rework code; nextflow target creates functionality in which there is no script-like resource

--- src/main/scala/com/dataintuitive/viash/functionality/resources/Executable.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/resources/Executable.scala
@@ -3,12 +3,13 @@ package com.dataintuitive.viash.functionality.resources
 import com.dataintuitive.viash.functionality._
 
 case class Executable(
+  text: Option[String] = None,
   name: Option[String] = None,
   path: Option[String] = None,
   is_executable: Boolean = true
 ) extends Script {
   val `type` = "executable"
-  val text = None
+//  val text = None
 
   val commentStr = "#"
 
