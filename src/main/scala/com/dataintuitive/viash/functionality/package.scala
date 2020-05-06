ee12094f266ac734fecdb656fda7343ece2a8796
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-06 16:29:36 +0200

    turn object type into object on its own

--- src/main/scala/com/dataintuitive/viash/functionality/package.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/package.scala
@@ -10,6 +10,23 @@ import functionality.resources._
 package object functionality {
 //  implicit val customConfig: Configuration = Configuration.default.withDefaults
 
+  // encoder and decoder for functiontype
+  implicit val encodeFunctionType: Encoder[FunctionType] = Encoder.instance {
+    ft => Json.fromString(ft.toString.toLowerCase())
+  }
+  implicit val decodeFunctionType: Decoder[FunctionType] = Decoder.instance {
+    cursor => cursor.value.as[String].map(s =>
+      s.toLowerCase() match {
+        case "asis" => AsIs
+        case "transform" => Transform
+        case "convert" => Convert
+        case "todir" => ToDir
+        case "join" => Join
+        case "unzip" => Unzip
+      }
+    )
+  }
+
   // encoder and decoder for Functionality
   implicit val encodeFunctionality: Encoder[Functionality] = deriveEncoder
   implicit val decodeFunctionality: Decoder[Functionality] = deriveDecoder
