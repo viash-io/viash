9d5b775d185bca480716ba613b419dba4cbfccf8
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-06 16:47:19 +0200

    remove flagValue and update tests

--- src/main/scala/com/dataintuitive/viash/functionality/dataobjects/BooleanObject.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/dataobjects/BooleanObject.scala
@@ -1,21 +1,49 @@
 package com.dataintuitive.viash.functionality.dataobjects
 
-case class BooleanObject(
+abstract class BooleanObject extends DataObject[Boolean] {
+  val flagValue: Option[Boolean]
+}
+
+case class BooleanObjectRegular(
     name: String,
     alternatives: Option[List[String]] = None,
     description: Option[String] = None,
     default: Option[Boolean] = None,
     required: Option[Boolean] = None,
-    flagValue: Option[Boolean] = None,
     tag: Option[String] = None,
     direction: Direction = Input,
     passthrough: Boolean = false
-) extends DataObject[Boolean] {
+) extends BooleanObject {
   override val `type` = "boolean"
 
-  require(
-    (default == None) != (flagValue == None),
-    message = s"Parameter $name: exactly one of default and flagValue must be defined, the other undefined."
-  )
+  val flagValue = None
 }
 
+case class BooleanObjectTrue(
+    name: String,
+    alternatives: Option[List[String]] = None,
+    description: Option[String] = None,
+    required: Option[Boolean] = None,
+    tag: Option[String] = None,
+    direction: Direction = Input,
+    passthrough: Boolean = false
+) extends BooleanObject {
+  override val `type` = "boolean_true"
+
+  val flagValue = Some(true)
+  val default = None
+}
+case class BooleanObjectFalse(
+    name: String,
+    alternatives: Option[List[String]] = None,
+    description: Option[String] = None,
+    required: Option[Boolean] = None,
+    tag: Option[String] = None,
+    direction: Direction = Input,
+    passthrough: Boolean = false
+) extends BooleanObject {
+  override val `type` = "boolean_false"
+
+  val flagValue = Some(false)
+  val default = None
+}
