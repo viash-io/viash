0aa68882863529904a559ee0a6f99ef58d2c99cb
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-05-08 18:25:25 +0200

    Atom viash updated, WIP but usable for export

--- atoms/viash/code.sh
+++ atoms/viash/code.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# PORTASH START
+# VIASH START
 par_functionality="functionality.yaml"
 par_platform="platform.yaml"
 par_mode="export"
-par_output="test/"
-PASSTHROUGH="-b" # passthrough
-# PORTASH END
+par_output="/tmp/viash/"
+# VIASH END
 
-java -jar $RESOURCES_DIR/viash-assembly-0.0.1.jar --functionality $par_functionality --platform $par_platform $par_mode --output $par_output
+java -jar $RESOURCES_DIR/viash-assembly-0.1.0.jar \
+  $par_mode \
+  --functionality $par_functionality \
+  --platform $par_platform \
+  --output $par_output
