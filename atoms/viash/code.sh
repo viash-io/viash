d1da0104de90d4ddf5d86907fe4aa6a10dac62f9
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-05-10 12:11:57 +0200

    Decent Viash atom, pointing to 0.1.0 build

--- atoms/viash/code.sh
+++ atoms/viash/code.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# VIASH START
-par_functionality="functionality.yaml"
-par_platform="platform.yaml"
-par_mode="export"
-par_output="/tmp/viash/"
-# VIASH END
-
-java -jar $RESOURCES_DIR/viash-assembly-0.1.0.jar \
-  $par_mode \
-  --functionality $par_functionality \
-  --platform $par_platform \
-  --output $par_output
+java -jar $RESOURCES_DIR/viash-assembly-0.1.0.jar $@
