4f12503f2dd75d728e8389bef14b5aee20358093
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-05-15 15:41:24 +0200

    update pandoc.sh in line with v0.1.0

--- temp/pandoc.sh
+++ temp/pandoc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# sbt assembly
+source temp/helper.sh
 
-JAVA="java -jar target/scala-2.12/viash-assembly-0.0.1.jar"
-
-$JAVA -f atoms/md_concat/functionality.yaml -p atoms/md_concat/platform_nextflow.yaml export -o output/pandoc_pipeline/modules/md_concat
-$JAVA -f atoms/md_add_toc/functionality.yaml -p atoms/md_add_toc/platform_nextflow.yaml export -o output/pandoc_pipeline/modules/md_add_toc
-$JAVA -f atoms/md_to_html/functionality.yaml -p atoms/md_to_html/platform_nextflow.yaml export -o output/pandoc_pipeline/modules/md_to_html
+viash export -f atoms/md_concat/functionality.yaml -p atoms/md_concat/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_concat
+viash export -f atoms/md_add_toc/functionality.yaml -p atoms/md_add_toc/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_add_toc
+viash export -f atoms/md_to_html/functionality.yaml -p atoms/md_to_html/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_to_html
