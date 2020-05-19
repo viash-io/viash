140b1e7be5dcfc81e0f124cf7512aedba1793483
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-19 19:42:50 +0200

    use testthat instead of assertthat

--- src/test/resources/testr/tests/check_outputs.R
+++ src/test/resources/testr/tests/check_outputs.R
@@ -1,9 +1,9 @@
 # assume tidyverse is installed
 options(tidyverse.quiet = TRUE)
 library(tidyverse)
-library(assertthat, warn.conflicts = FALSE)
+library(testthat, warn.conflicts = FALSE)
 
-if (Sys.getenv("VIASH_PLATFORM") == "docker") {
+if (identical(Sys.getenv("VIASH_PLATFORM"), "docker")) {
   docker_args <- c("--data", getwd())
   data_dir <- "/data"
 } else {
@@ -11,50 +11,51 @@ if (Sys.getenv("VIASH_PLATFORM") == "docker") {
   data_dir <- getwd()
 }
 
-cat(">>> Building component\n")
-out <- processx::run("testr", "---setup")
+test_that("Building component", {
+  out <- processx::run("testr", "---setup")
+})
 
-cat(">>> Checking whether output is correct\n")
-out <- processx::run("testr", c(
-  "help", "--real_number", "10.5", "--whole_number=10", "-s", "you shall#not$pass",
-  "--truth", "--output", paste0(data_dir, "/output.txt"), "--log", paste0(data_dir, "/log.txt"),
-  "--optional", "foo", "--optional_with_default", "bar", docker_args
-))
 
-assert_that(file.exists("output.txt"))
-output <- readr::read_file("output.txt")
-assert_that(
-  grepl('input: "help"', output),
-  grepl('real_number: "10.5"', output),
-  grepl('whole_number: "10"', output),
-  grepl('s: "you shall#not\\$pass"', output),
-  grepl('truth: "TRUE"', output),
-  grepl('output: ".*/output.txt"', output),
-  grepl('log: ".*/log.txt"', output),
-  grepl('optional: "foo"', output),
-  grepl('optional_with_default: "bar"', output)
-)
+test_that("Checking whether output is correct", {
+  out <- processx::run("testr", c(
+    "help", "--real_number", "10.5", "--whole_number=10", "-s", "you shall#not$pass",
+    "--truth", "--optional", "foo", "--optional_with_default", "bar",
+    "--output", paste0(data_dir, "/output.txt"), "--log", paste0(data_dir, "/log.txt"), docker_args
+  ))
+  
+  expect_true(file.exists("output.txt"))
+  
+  output <- readr::read_file("output.txt")
+  expect_match(output, 'input: "help"')
+  expect_match(output, 'input: "help"')
+  expect_match(output, 'real_number: "10.5"')
+  expect_match(output, 'whole_number: "10"')
+  expect_match(output, 's: "you shall#not\\$pass"')
+  expect_match(output, 'truth: "TRUE"')
+  expect_match(output, 'output: ".*/output.txt"')
+  expect_match(output, 'log: ".*/log.txt"')
+  expect_match(output, 'optional: "foo"')
+  expect_match(output, 'optional_with_default: "bar"')
+  
+  expect_true(file.exists("log.txt"))
+  log <- readr::read_file("log.txt")
+  expect_match(log, 'Parsed input arguments.')
+})
 
-assert_that(file.exists("log.txt"))
-log <- readr::read_file("log.txt")
-assert_that(
-  grepl('Parsed input arguments.', log)
-)
 
-cat(">>> Checking whether output is correct with minimal parameters\n")
-out <- processx::run("testr", c(
-  "test", "--real_number", "123.456", "--whole_number=789", "-s", "my weird string", docker_args
-))
-
-output <- out$stdout
-assert_that(
-  grepl('input: "test"', output),
-  grepl('real_number: "123.456"', output),
-  grepl('whole_number: "789"', output),
-  grepl('s: "my weird string"', output),
-  grepl('truth: "FALSE"', output),
-  grepl('optional_with_default: "The default value."', output),
-  grepl('Parsed input arguments', output)
-)
+test_that("Checking whether output is correct with minimal parameters", {
+  out <- processx::run("testr", c(
+    "test", "--real_number", "123.456", "--whole_number=789", "-s", "my weird string", docker_args
+  ))
+  
+  output <- out$stdout
+  expect_match(output, 'input: "test"')
+  expect_match(output, 'real_number: "123.456"')
+  expect_match(output, 'whole_number: "789"')
+  expect_match(output, 's: "my weird string"')
+  expect_match(output, 'truth: "FALSE"')
+  expect_match(output, 'optional_with_default: "The default value."')
+  expect_match(output, 'Parsed input arguments')
+})
 
 
