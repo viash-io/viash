460de5165fea4988e22f82d78a6c94482ee5fdab
Robrecht Cannoodt <rcannood@gmail.com>
2020-05-19 19:29:00 +0200

    add tests for testbash and testr

---
+++ src/test/resources/testbash/tests/check_outputs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# print all commands and exit when one of the commands produces an error
+set -ex
+
+if [ "$VIASH_PLATFORM" == "docker" ]; then
+  # Docker platform detected, have to specify a few variables
+  docker_args="--data `pwd`"
+  data_dir="/data"
+else
+  data_dir=`pwd`
+fi
+
+echo ">>> Building component"
+testbash ---setup > /dev/null
+
+echo ">>> Checking whether output is correct"
+testbash $docker_args \
+  help --real_number 10.5 --whole_number=10 -s "a string with spaces" --truth \
+  --output $data_dir/output.txt --log $data_dir/log.txt \
+  --optional foo --optional_with_default bar --passthrough='you shall#not$pass' --passthroughbool
+
+[[ ! -f output.txt ]] && echo "Output file could not be found!" && exit 1
+grep -q 'input: "help"' output.txt
+grep -q 'real_number: "10.5"' output.txt
+grep -q 'whole_number: "10"' output.txt
+grep -q 's: "a string with spaces"' output.txt
+grep -q 'truth: "true"' output.txt
+grep -q 'output: ".*/output.txt"' output.txt
+grep -q 'log: ".*/log.txt"' output.txt
+grep -q 'optional: "foo"' output.txt
+grep -q 'optional_with_default: "bar"' output.txt
+grep -q 'passthrough: "you shall#not$pass"' output.txt
+grep -q 'passthroughbool: "true"' output.txt
+
+[[ ! -f log.txt ]] && echo "Log file could not be found!" && exit 1
+grep -q 'Parsed input arguments.' log.txt
+
+echo ">>> Checking whether output is correct with minimal parameters"
+testbash $docker_args \
+  test --real_number 123.456 --whole_number=789 -s 'my$weird#string' \
+  > output2.txt
+
+[[ ! -f output2.txt ]] && echo "Output file could not be found!" && exit 1
+grep -q 'input: "test"' output2.txt
+grep -q 'real_number: "123.456"' output2.txt
+grep -q 'whole_number: "789"' output2.txt
+grep -q 's: "my$weird#string"' output2.txt
+grep -q 'truth: "false"' output2.txt
+grep -q 'output: ""' output2.txt
+grep -q 'log: ""' output2.txt
+grep -q 'optional: ""' output2.txt
+grep -q 'optional_with_default: "The default value."' output2.txt
+grep -q 'passthrough: ""' output2.txt
+grep -q 'passthroughbool: "false"' output2.txt
+
+echo ">>> Test finished successfully"
+exit 0
