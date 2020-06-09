95d1e07f1da7809d25b10309265cb5723d84d948
Robrecht Cannoodt <rcannood@gmail.com>
2020-06-09 11:22:26 +0200

    add negative tests

---
+++ src/test/resources/testbash/tests/fail.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+
+echo ">>> This test should always fail"
+$RESOURCES_DIR/testbash "$RESOURCES_DIR/missingresource" --real_number abc --whole_number abc -s "a string with spaces" --truth \
+  --output ./output.txt --log ./log.txt \
+  --optional foo --optional_with_default bar --passthrough='you shall#not$pass' --passthroughbool
+
+[ $? -eq 0 ] && echo "Command should have failed, but didn't." && exit 1
+
+echo Done
