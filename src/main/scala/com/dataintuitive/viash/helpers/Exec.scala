b6e5f0d7b66e8727de93c53d803117645c570342
Robrecht Cannoodt <rcannood@gmail.com>
2020-06-23 10:02:09 +0200

    fix test

--- src/main/scala/com/dataintuitive/viash/helpers/Exec.scala
+++ src/main/scala/com/dataintuitive/viash/helpers/Exec.scala
@@ -5,15 +5,40 @@ import java.io.File
 import java.nio.file.{Paths, Files}
 import scala.reflect.io.Directory
 
+import sys.process.{Process, ProcessLogger}
+import java.io.{ByteArrayOutputStream, PrintWriter, FileWriter, File}
+import java.nio.file.{Paths, Files}
+
+case class ExecOutput(command: Seq[String], exitValue: Int, output: String)
+
 object Exec {
-  def run(commands: Seq[String], cwd: Option[File] = None, extraEnv: Seq[(String, String)] = Nil) = {
+  def run(command: Seq[String], cwd: Option[File] = None, extraEnv: Seq[(String, String)] = Nil) = {
     try {
-      Process(commands, cwd = cwd, extraEnv = extraEnv: _*).!!
+      Process(command, cwd = cwd, extraEnv = extraEnv: _*).!!
     } catch {
       case e: Throwable => {
         println(e.getMessage)
         throw e
       }
+    }
+  }
+
+  def run2(command: Seq[String], cwd: Option[File] = None, extraEnv: Seq[(String, String)] = Nil, loggers: Seq[String => Unit] = Nil) = {
+     // run command, collect output
+    val stream = new ByteArrayOutputStream
+    val printwriter = new PrintWriter(stream)
+
+    val logger = (s: String) => {
+      printwriter.println(s)
+      for (log <- loggers) log(s)
+    }
+
+    // run command, collect output
+    try {
+      val exitValue = Process(command, cwd = cwd, extraEnv = extraEnv: _*).!(ProcessLogger(logger, logger))
+      ExecOutput(command, exitValue, stream.toString)
+    } finally {
+      printwriter.close()
     }
   }
 
