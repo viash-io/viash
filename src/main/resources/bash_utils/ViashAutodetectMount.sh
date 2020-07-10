acb848a5728f5d3b0ca15ebaf9792241b7b9db5e
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-10 10:12:16 +0200

    rework viash autodetect mount

--- src/main/resources/bash_utils/ViashAutodetectMount.sh
+++ src/main/resources/bash_utils/ViashAutodetectMount.sh
@@ -1,24 +1,28 @@
 # ViashAutodetectMount: auto configuring docker mounts from parameters
-# $1                  : The parameter name
-# $2                  : The parameter value
+# $1                  : The parameter value
 # returns             : New parameter
 # $VIASH_EXTRA_MOUNTS : Added another parameter to be passed to docker
 # examples:
-#   ViashAutodetectMount --foo /path/to/bar
-#   -> sets VIASH_EXTRA_MOUNTS to "-v /path/to:/viash_automount/foo"
-#   -> returns /viash_automount/foo/bar
+#   ViashAutodetectMount /path/to/bar
+#   -> sets VIASH_DOCKER_MOUNTS to "-v /path/to:/viash_automount/path/to"
+#   -> returns /viash_automount/path/to/bar
 function ViashAutodetectMount {
-  local PARNAME=`echo $1 | sed 's#^-*##'`
-  local ABSPATH=`ViashAbsolutePath $2`
-  local NEWMOUNT
-  local NEWNAME
-  if [ -d $ABSPATH ]; then
-    NEWMOUNT=$ABSPATH
-    NEWNAME=""
+  local abs_path=`ViashAbsolutePath $1`
+  local mount_source
+  local base_name
+  if [ -d $abs_path ]; then
+    mount_source=$abs_path
+    base_name=""
   else
-    NEWMOUNT=`dirname $ABSPATH`
-    NEWNAME=`basename $ABSPATH`
+    mount_source=`dirname $abs_path`
+    base_name=`base_name $abs_path`
   fi
-  VIASH_EXTRA_MOUNTS="-v \"$NEWMOUNT:/viash_automount/$PARNAME\" $VIASH_EXTRA_MOUNTS"
-  echo "\"/viash_automount/$PARNAME/$NEWNAME\""
+  local mount_target="/viash_automount/$mount_source"
+  local new_data="-v \"$mount_source:$mount_target\""
+  if [ -z "$var" ]; then
+    VIASH_DOCKER_MOUNTS=$new_data
+  else
+    VIASH_DOCKER_MOUNTS="$VIASH_DOCKER_MOUNTS $new_data"
+  fi
+  echo "\"$mount_target/$base_name\""
 }
