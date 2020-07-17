af58cdefe769829c02f30736c990805d69857423
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-17 13:33:35 +0200

    assume build is handled by make && make install beforehand

--- temp/e2etests.sh
+++ temp/e2etests.sh
@@ -4,7 +4,7 @@ source temp/helper.sh
 
 # run_tests
 # assembly
-assembly_without_testing
+# assembly_without_testing
 
 # viash_export_all atoms/ output/
 viash_export_all src/test/resources/ output/
