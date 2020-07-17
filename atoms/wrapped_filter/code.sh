4124de226fe9841896ee2046651571b962eab738
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-17 13:42:31 +0200

    update atoms

--- atoms/wrapped_filter/code.sh
+++ atoms/wrapped_filter/code.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # VIASH START
-par_input="input.txt"
+par_input="build.sbt"
 par_output="output.txt"
-par_filter="AString"
-PASSTHROUGH="-b" # passthrough
+par_filter="scala"
+par_number_nonblank="true"
 # VIASH END
 
 echo "## Reading from $par_input"
 echo "## Filtering for $par_filter"
 echo "## Writing to $par_output"
-echo "## Additional parameters? $CMDARGS"
-echo ">> Running cat $PASSTHROUGH $par_input | grep $par_filter > $par_output"
-cat $PASSTHROUGH "$par_input" | grep "$par_filter" > "$par_output"
+echo ">> Running cat $extra_args $par_input | grep $par_filter > $par_output"
+
+if [ $par_number_nonblank == "true" ]; then
+  extra_args="$extra_args -b"
+fi
+cat $extra_args "$par_input" | grep "$par_filter" > "$par_output"
