640dc4bbacc5434a770a299a96448f5606fc1aa1
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-21 23:09:15 +0200

    fix requirements

--- src/main/scala/com/dataintuitive/viash/platforms/requirements/Requirements.scala
+++ src/main/scala/com/dataintuitive/viash/platforms/requirements/Requirements.scala
@@ -3,4 +3,11 @@ package com.dataintuitive.viash.platforms.requirements
 trait Requirements {
   val `type`: String
   def installCommands: List[String]
+  def dockerCommands: Option[String] = {
+    if (installCommands.isEmpty) {
+      None
+    } else {
+      Some(installCommands.mkString("RUN ", " && \\\n  ", "\n"))
+    }
+  }
 }
