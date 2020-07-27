8c20cc7d47c057f2c77255fa3a5dabef5ad05d7c
Toni Verbeiren <toni.verbeiren@gmail.com>
2020-07-27 16:48:11 +0200

    Write meta.yaml to exported dir

---
+++ src/main/scala/com/dataintuitive/viash/meta/Meta.scala
@@ -0,0 +1,51 @@
+package com.dataintuitive.viash.meta
+
+import io.circe.{ Decoder, Encoder, Json }
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+
+import com.dataintuitive.viash.functionality.resources.PlainFile
+import com.dataintuitive.viash.functionality._
+import com.dataintuitive.viash.platforms._
+import io.circe.yaml.Printer
+
+case class Meta(
+  version: String,
+  fun: Functionality,
+  platform: Platform,
+  functionalityPath: String,
+  platformPath: String,
+  outputPath: String,
+  executablePath: String
+  ) {
+    def info =
+      s"""version: ${version}
+         |functionalityPath: ${functionalityPath}
+         |platformPath: ${platformPath}
+         |outputPath: ${outputPath}
+         |executablePath: ${executablePath}""".stripMargin
+
+    def yaml = {
+
+      val strippedMeta = StrippedMeta(
+        version,
+        functionalityPath,
+        platformPath,
+        outputPath,
+        executablePath
+      )
+
+      val encoded = encodeNested(Nested(strippedMeta, fun, platform))
+
+      // Options: https://github.com/circe/circe-yaml/blob/master/src/main/scala/io/circe/yaml/Printer.scala
+      val pretty = Printer(
+        dropNullKeys = true,
+        mappingStyle = Printer.FlowStyle.Block,
+        splitLines = true
+      ).pretty(encoded)
+
+      pretty
+
+    }
+
+    val resource = PlainFile(Some("meta.yaml"), None, Some(yaml))
+  }
