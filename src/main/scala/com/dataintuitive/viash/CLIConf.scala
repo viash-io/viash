82f88206cf47a52057c4fd29ff9871d4917a7e89
Robrecht Cannoodt <rcannood@gmail.com>
2020-08-12 10:29:09 +0200

    clean up cli

--- src/main/scala/com/dataintuitive/viash/CLIConf.scala
+++ src/main/scala/com/dataintuitive/viash/CLIConf.scala
@@ -1,6 +1,6 @@
 package com.dataintuitive.viash
 
-import org.rogach.scallop.{ScallopConf, Subcommand}
+import org.rogach.scallop.{Scallop, ScallopConf, Subcommand, ScallopHelpFormatter}
 
 trait WithFunctionality { _: ScallopConf =>
   val functionality = opt[String](
@@ -27,6 +27,8 @@ trait WithTemporary { _: ScallopConf =>
 class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
   version(s"${Main.name} ${Main.version} (c) 2020 Data Intuitive")
 
+  appendDefaultToDescription = true
+
   banner(s"""
     |viash is a spec and a tool for defining execution contexts and converting execution instructions to concrete instantiations.
     |
@@ -35,36 +37,33 @@ class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
     |  viash export -f functionality.yaml [-p platform.yaml] -o output [-m]
     |  viash test -f functionality.yaml [-p platform.yaml] [-v] [-k]
     |
-    |API Documentation:
+    |Check the help of a subcommand for more information, or the API available at:
     |  https://github.com/data-intuitive/viash_docs
     |
     |Arguments:""".stripMargin)
 
   val run = new Subcommand("run") with WithFunctionality with WithPlatform with WithTemporary {
-    banner(s"""`viash run` executes a viash component. From the provided functionality.yaml viash generates a temporary executable and immediately executes it with the given parameters.
+    banner(s"""viash run
+      |Executes a viash component. From the provided functionality.yaml, viash generates a temporary executable and immediately executes it with the given parameters.
       |
       |Usage:
-      |  viash run -f fun.yaml [-p plat.yaml] [-k] [-- --params --to component]
+      |  viash run -f functionality.yaml [-p platform.yaml] [-k] [-- --params --to component]
       |
       |Arguments:""".stripMargin)
 
     footer(s"""
       |The temporary directory can be altered by setting the VIASH_TEMP directory. Example:
       |  export VIASH_TEMP=/home/myuser/.viash_temp
-      |  viash run -f fun.yaml -k
-      |
-      |===============================================""".stripMargin)
+      |  viash run -f fun.yaml -k""".stripMargin)
   }
   val export = new Subcommand("export") with WithFunctionality with WithPlatform {
-    banner(s"""`viash export` generates an executable from the functionality and platform meta information.
+    banner(s"""viash export
+      |Generate an executable from the functionality and platform meta information.
       |
       |Usage:
       |  viash export -f functionality.yaml [-p platform.yaml] -o output [-m]
       |
       |Arguments:""".stripMargin)
-
-    footer(s"""
-      |===============================================""".stripMargin)
 
     val meta = opt[Boolean](
         name = "meta",
@@ -79,7 +78,8 @@ class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
     )
   }
   val test = new Subcommand("test") with WithFunctionality with WithPlatform with WithTemporary {
-    banner(s"""`viash test` runs the tests as defined in the functionality.yaml. Check the documentation for more information on how to write tests.
+    banner(s"""viash test
+      |Run the tests as defined in the functionality.yaml. Check the documentation for more information on how to write tests.
       |
       |Usage:
       |  viash test -f functionality.yaml [-p platform.yaml] [-v] [-k]
@@ -96,14 +96,14 @@ class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
     footer(s"""
       |The temporary directory can be altered by setting the VIASH_TEMP directory. Example:
       |  export VIASH_TEMP=/home/myuser/.viash_temp
-      |  viash run -f fun.yaml -k
-      |
-      |===============================================""".stripMargin)
+      |  viash run -f fun.yaml -k""".stripMargin)
   }
 
   addSubcommand(run)
   addSubcommand(export)
   addSubcommand(test)
+
+  shortSubcommandsHelp(true)
 
   verify()
 }
