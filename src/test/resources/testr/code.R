9bce8e9342cd15713fe7ae85579d9468dba101dd
Robrecht Cannoodt <rcannood@gmail.com>
2020-08-20 12:16:03 +0200

    merge testr files

--- src/test/resources/testr/code.R
+++ src/test/resources/testr/code.R
@@ -1,17 +1,76 @@
-### VIASH START
-par <- list(
-  input = "input.txt",
-  real_number = 123.456,
-  whole_number = 789,
-  s = "...",
-  truth = TRUE,
-  output = "output.txt",
-  log = "log.txt",
-  optional = "help",
-  optional_with_default = "me"
-)
-### VIASH END
-par$resources_dir <- resources_dir
+#' functionality:
+#'   name: testr
+#'   description: | 
+#'     Prints out the parameter values.
+#'     Checking what happens with multiline descriptions.
+#'   arguments: 
+#'   - name: "input"
+#'     type: file
+#'     description: An input file with positional arguments.
+#'     direction: input
+#'   - name: "--real_number"
+#'     type: double
+#'     description: A real number with positional arguments.
+#'     required: true
+#'   - name: "--whole_number"
+#'     type: integer
+#'     description: A whole number with a standard flag.
+#'     required: true
+#'   - name: "-s"
+#'     type: string
+#'     description: A sentence or word with a short flag.
+#'     required: true
+#'   - name: "--truth"
+#'     type: boolean_true
+#'     description: A switch flag.
+#'   - name: "--output"
+#'     alternatives: ["-o"]
+#'     type: file
+#'     description: Write the parameters to a json file.
+#'     direction: output
+#'   - name: "--log"
+#'     type: file
+#'     description: An optional log file.
+#'     direction: log
+#'   - name: "--optional"
+#'     type: string
+#'     description: An optional string.
+#'   - name: "--optional_with_default"
+#'     type: string
+#'     default: "The default value."
+#'   - name: "--multiple"
+#'     type: string
+#'     multiple: true
+#'   - name: "multiple_pos"
+#'     type: string
+#'     multiple: true
+#'   tests:
+#'   - type: r_script
+#'     path: tests/check_outputs.R
+#' platforms:
+#' - type: native
+#'   r:
+#'     cran: 
+#'     - optparse
+#' - type: docker
+#'   image: rocker/tidyverse
+#'   target_image: "viash_test/r"
+#'   r:
+#'     cran: 
+#'     - optparse
+#'     github:
+#'     - dynverse/dynutils@devel
+#'     bioc:
+#'     - limma
+#'   apt:
+#'     packages:
+#'     - libhdf5-serial-dev
+#'   volumes:
+#'   - name: data
+#'     mount: /data
+#'   workdir: /app
+#' - type: nextflow
+#'   image: rocker/tidyverse
 
 write_fun <- function(file, ...) {
   str <- paste0(..., sep = "")
