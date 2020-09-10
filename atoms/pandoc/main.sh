b04afbfbd2c074f19534f8e4ffb606cfc9d9156a
Robrecht Cannoodt <rcannood@gmail.com>
2020-09-10 09:12:44 +0200

    remove lingering files

--- atoms/pandoc/main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-uname -a
--- temp/pandoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source temp/helper.sh
-
-viash export -f atoms/md_concat/functionality.yaml -p atoms/md_concat/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_concat
-viash export -f atoms/md_add_toc/functionality.yaml -p atoms/md_add_toc/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_add_toc
-viash export -f atoms/md_to_html/functionality.yaml -p atoms/md_to_html/platform_nextflow.yaml -o output/pandoc_pipeline/modules/md_to_html
