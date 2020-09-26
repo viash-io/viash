e68423c59938fa4c9ac7cea9c1696eda251adb5b
Hendrik Cannoodt <hendrik@griffintech.be>
2020-09-26 22:11:12 +0200

    Use new format of test output, start of second test to check --keep option
    Add method to facilitate capturing stdout from the test method, place performing the test into the test function, this way everything is much cleaner and well isolated.
    Second test to be further implemented

--- src/test/scala/com/dataintuitive/viash/E2EMainBashDocker.scala
+++ src/test/scala/com/dataintuitive/viash/E2EMainBashDocker.scala
@@ -19,30 +19,55 @@ class E2EMainBashDocker extends FunSuite with BeforeAndAfterAll {
   // parse functionality from file
   private val functionality = Functionality.parse(IO.uri(funcFile))
 
-  // convert testbash
-  private val params = Array(
-    "test",
-    "-f", funcFile,
-    "-p", platFile
-  )
-
-  private val os = new ByteArrayOutputStream()
-  Console.withOut(os) {
-
-    Main.main(params)
-
-  }
-
-  private val stdout = os.toString()
-  Console.print("stdout: -->")
-  Console.print(stdout)
-  Console.println("<--")
 
   test("Check standard test output for typical outputs") {
-    assert(stdout.contains("SUCCESS! All 3 out of 3 test scripts succeeded!"))
-    assert(stdout.contains("Cleaning up temporary files"))
+    val testText = executeMainAndCaptureStdOut(
+      Array(
+        "test",
+        "-f", funcFile,
+        "-p", platFile
+      ))
+    assert(testText.contains("Running tests in temporary directory: "))
+    assert(testText.contains("SUCCESS! All 3 out of 3 test scripts succeeded!"))
+    assert(testText.contains("Cleaning up temporary directory"))
   }
 
+
+
+  test("Check output in case of --keep is specified") {
+    val testText = executeMainAndCaptureStdOut(
+      Array(
+        "test",
+        "-f", funcFile,
+        "-p", platFile,
+        "--keep"
+      ))
+    assert(testText.contains("Running tests in temporary directory: "))
+    assert(testText.contains("SUCCESS! All 3 out of 3 test scripts succeeded!"))
+    assert(!testText.contains("Cleaning up temporary directory"))
+
+    // Running tests in temporary directory: '/tmp/viash_test_testbash9467890914539760538'
+    // Check folder exists
+    // Check file in folder
+    // remove temporary directory
+  }
+
+
+
+  def executeMainAndCaptureStdOut(args: Array[String]) : String = {
+    val os = new ByteArrayOutputStream()
+    Console.withOut(os) {
+
+      Main.main(args)
+
+    }
+
+    val stdout = os.toString()
+    //Console.print("stdout: -->")
+    //Console.print(stdout)
+    //Console.println("<--")
+    return stdout
+  }
 
   override def afterAll() {
     IO.deleteRecursively(temporaryFolder)
