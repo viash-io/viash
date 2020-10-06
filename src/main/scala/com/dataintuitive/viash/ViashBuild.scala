c2f751f53588afb3a6fc49054b41474a377c3add
Robrecht Cannoodt <rcannood@gmail.com>
2020-10-06 20:32:30 +0200

    add some comments and change the printer such that the order is preserved

--- src/main/scala/com/dataintuitive/viash/ViashBuild.scala
+++ src/main/scala/com/dataintuitive/viash/ViashBuild.scala
@@ -22,16 +22,22 @@ object ViashBuild {
     val dir = new java.io.File(output)
     dir.mkdirs()
 
-    // create Config Resource
+    // create a yaml printer for writing the viash.yaml file
     // Options: https://github.com/circe/circe-yaml/blob/master/src/main/scala/io/circe/yaml/Printer.scala
     val printer = Printer(
+      preserveOrder = true,
       dropNullKeys = true,
       mappingStyle = Printer.FlowStyle.Block,
       splitLines = true
     )
 
+    // get the path of where the executable will be written to
     val exec_path = fun.mainScript.map(scr => Paths.get(output, scr.name.get).toString)
 
+    // change the config object before writing to yaml:
+    // * add more info variables
+    // * remove other platforms other than the one finally used
+    // * override namespace in functionality
     val strippedConfig = config.copy(
       info = config.info.map(_.copy(
         output_path = Some(output),
@@ -42,18 +48,24 @@ object ViashBuild {
       functionality = config.functionality.copy(namespace = namespace)
     )
 
+    // add yaml to resources
+    val configYamlStr = printer.pretty(encodeConfig(strippedConfig))
+    // TODO: manually change text to a nice format?
     val configYaml = PlainFile(
       name = Some("viash.yaml"),
-      text = Some(printer.pretty(encodeConfig(strippedConfig)))
+      text = Some(configYamlStr)
     )
 
+    // write resources to output directory
     IO.writeResources(configYaml :: fun.resources.getOrElse(Nil), dir)
 
+    // if '--setup' was passed, run './executable ---setup'
     if (setup && exec_path.isDefined) {
       val cmd = Array(exec_path.get, "---setup")
       val _ = Process(cmd).!(ProcessLogger(println, println))
     }
 
+    // if '-m' was passed, print some yaml about the created output fiels
     if (printMeta) {
       println(config.info.get.consoleString)
     }
