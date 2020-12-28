11a3bcc95d84c837500d0ec2faaef4068dc0d9ab
Hendrik Cannoodt <hendrik@griffintech.be>
2020-12-28 11:58:03 +0100

    Test boolean_false in base test script
    previously only boolean_true was tested

--- src/test/resources/testbash/tests/check_outputs.sh
+++ src/test/resources/testbash/tests/check_outputs.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo ">>> Checking whether output is correct"
-./testbash "NOTICE" --real_number 10.5 --whole_number=10 -s "a string with spaces" --truth \
+./testbash "NOTICE" --real_number 10.5 --whole_number=10 -s "a string with spaces" --truth --falsehood \
   --optional foo --optional_with_default bar \
   a b c d \
   --output ./output.txt --log ./log.txt \
@@ -15,6 +15,7 @@ grep -q 'real_number: |10.5|' output.txt
 grep -q 'whole_number: |10|' output.txt
 grep -q 's: |a string with spaces|' output.txt
 grep -q 'truth: |true|' output.txt
+grep -q 'falsehood: |false|' output.txt
 grep -q 'output: |.*/output.txt|' output.txt
 grep -q 'log: |.*/log.txt|' output.txt
 grep -q 'optional: |foo|' output.txt
@@ -39,6 +40,7 @@ grep -q 'real_number: |123.456|' output2.txt
 grep -q 'whole_number: |789|' output2.txt
 grep -q 's: |my$weird#string|' output2.txt
 grep -q 'truth: |false|' output2.txt
+grep -q 'falsehood: |true|' output2.txt
 grep -q 'output: ||' output2.txt
 grep -q 'log: ||' output2.txt
 grep -q 'optional: ||' output2.txt
