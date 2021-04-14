ab2f1fc80ffecb5ad315b1689dac03a1eae542ce
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-14 11:59:50 +0200

    update project_doc: `viash ns list` returns a list instead of a { config: <list> }.

--- src/viash/project_doc/script.sh
+++ src/viash/project_doc/script.sh
@@ -7,24 +7,36 @@ function output {
 echo "# Repository Overview" > $par_output
 output ""
 
-namespaces=`viash ns list -s $par_repo/"$par_src" | yq e '.configs[].functionality.namespace' - | uniq`
+namespaces=`viash ns list -s $par_repo/"$par_src" | yq e '.[].functionality.namespace' - | uniq`
 
+tempscript=$(mktemp ".tmp_viash_config_view_XXXXXX")
+function clean_up {
+  [[ -f "$tempscript" ]] && rm "$tempscript"
+}
+trap clean_up EXIT
+    
 for ns in $namespaces; do
   echo "> Generating documentation for namespace $ns"
   output "## $ns"
   output ""
-  components=`viash ns list -n $ns -s $par_repo/$par_src | yq e '.configs[].functionality.name' - | uniq`
-  for comp in $components; do
-    echo "  > Generating documentation for $ns/$comp"
-    output "### $comp"
+  comp_files=`viash ns list -n $ns -s $par_repo/$par_src | yq e '.[].info.config' - | uniq`
+  for comp_file in $comp_files; do
+    viash config view "$comp_file" > "$tempscript"
+    
+    comp_name=`yq e '.functionality.name' "$tempscript"`
+    comp_desc=`yq e '.functionality.description' "$tempscript"`
+    echo "  > Generating documentation for $ns/$comp_name"
+    output "### $comp_name"
     output ""
-    output `viash ns list -q "$comp" -s $par_repo/$par_src | yq e '.configs[0].functionality.description' -`
+    output $comp_desc
     output ""
     output '```sh'
-    output "$ viash run $par_repo/$par_src/$ns/$comp/*.vsh.yaml -- -h"
-    viash run $par_repo/$par_src/$ns/$comp/*.vsh.yaml -- -h >> $par_output
+    output "$ viash run $comp_file -- -h"
+    viash run $comp_file -- -h >> $par_output
     output '```'
     output ""
+    
+    clean_up
   done
 done
 
