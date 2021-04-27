05b19ba29bbc89c36730903b5d4b96d8506e5d48
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-27 11:39:43 +0200

    do not check execution times

--- src/test/scala/com/dataintuitive/viash/MainNSTestNativeSuite.scala
+++ src/test/scala/com/dataintuitive/viash/MainNSTestNativeSuite.scala
@@ -22,21 +22,21 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
 
     val steps = List(
       ("start",""),
-      ("build_executable","\\s*0\\s*0\\s*SUCCESS"),
-      ("test\\.sh","\\s*0\\s*0\\s*SUCCESS")
+      ("build_executable","\\s*0\\s*\\d+\\s*SUCCESS"),
+      ("test\\.sh","\\s*0\\s*\\d+\\s*SUCCESS")
     )
 
     // Test all 'normal' steps for components
     for (component ← components) {
       for ((step, resultPattern) ← steps) {
         val regex = s"""testns\\s*$component\\s*native\\s*$step$resultPattern""".r
-        assert(regex.findFirstIn(testText).isDefined, s"-->${regex.toString}<-->$testText<--")
+        assert(regex.findFirstIn(testText).isDefined, s"\nRegex: '${regex.toString}'; text: \n$testText")
       }
     }
 
     // Check for the one failing test of ns_divide
-    val regexFail = s"""testns\\s*ns_divide\\s*native\\s*test_div0\\.sh\\s*1\\s*0\\s*ERROR""".r
-    assert(regexFail.findFirstIn(testText).isDefined, s"-->${regexFail.toString}<-->$testText<--")
+    val regexFail = s"""testns\\s*ns_divide\\s*native\\s*test_div0\\.sh\\s*1\\s*\\d+\\s*ERROR""".r
+    assert(regexFail.findFirstIn(testText).isDefined, s"\nRegex: '${regexFail.toString}'; text: \n$testText")
   }
 
 }
