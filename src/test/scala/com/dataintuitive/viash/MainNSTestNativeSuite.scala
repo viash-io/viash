b577db55a62a29cbc656f748e3ff87b4fe80124a
Hendrik Cannoodt <hendrik@griffintech.be>
2021-05-13 15:31:40 +0200

    Test condition of missing tests in namespaces
    Added namespace component 'ns_power' without tests defined
    Refactored code to define the expected steps instead of having a base set with exceptions. The exceptions were becoming bigger than the base code.

--- src/test/scala/com/dataintuitive/viash/MainNSTestNativeSuite.scala
+++ src/test/scala/com/dataintuitive/viash/MainNSTestNativeSuite.scala
@@ -14,6 +14,34 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
   private val temporaryFolder = IO.makeTemp("viash_ns_test_tsv")
   private val tempFolStr = temporaryFolder.toString
 
+  val stepsSuccess = List(
+    ("start",""),
+    ("build_executable","\\s*0\\s*\\d+\\s*SUCCESS"),
+    ("test\\.sh","\\s*0\\s*\\d+\\s*SUCCESS")
+  )
+
+  val stepsFailure = List(
+    ("start",""),
+    ("build_executable","\\s*0\\s*\\d+\\s*SUCCESS"),
+    ("test\\.sh","\\s*0\\s*\\d+\\s*SUCCESS"),
+    ("test_div0\\.sh","\\s*1\\s*\\d+\\s*ERROR"),
+  )
+
+  val stepsMissing = List(
+    ("start",""),
+    ("build_executable","\\s*0\\s*\\d+\\s*SUCCESS"),
+    ("tests","\\s*-1\\s*\\d+\\s*MISSING")
+  )
+
+  val components = List(
+    ("ns_add", stepsSuccess),
+    ("ns_subtract", stepsSuccess),
+    ("ns_multiply", stepsSuccess),
+    ("ns_divide", stepsFailure),
+    ("ns_power", stepsMissing)
+  )
+
+
   test("Check namespace test output") {
     val testText = TestHelper.testMain(
       Array(
@@ -21,30 +49,12 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
         "--src", nsPath
       ))
 
-    val components = List(
-      "ns_add",
-      "ns_subtract",
-      "ns_multiply",
-      "ns_divide"
-    )
-
-    val steps = List(
-      ("start",""),
-      ("build_executable","\\s*0\\s*\\d+\\s*SUCCESS"),
-      ("test\\.sh","\\s*0\\s*\\d+\\s*SUCCESS")
-    )
-
-    // Test all 'normal' steps for components
-    for (component ← components) {
+    for ((component, steps) ← components) {
       for ((step, resultPattern) ← steps) {
         val regex = s"""testns\\s*$component\\s*native\\s*$step$resultPattern""".r
         assert(regex.findFirstIn(testText).isDefined, s"\nRegex: '${regex.toString}'; text: \n$testText")
       }
     }
-
-    // Check for the one failing test of ns_divide
-    val regexFail = s"""testns\\s*ns_divide\\s*native\\s*test_div0\\.sh\\s*1\\s*\\d+\\s*ERROR""".r
-    assert(regexFail.findFirstIn(testText).isDefined, s"\nRegex: '${regexFail.toString}'; text: \n$testText")
   }
 
   test("Check namespace test output with tsv option") {
@@ -57,37 +67,25 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
         "--src", nsPath
       ))
 
-    val components = List(
-      "ns_add",
-      "ns_subtract",
-      "ns_multiply",
-      "ns_divide"
-    )
-
-    val steps = List(
-      ("build_executable","\\t0\\t\\d+\\tSUCCESS"),
-      ("test\\.sh","\\t0\\t\\d+\\tSUCCESS")
-    )
-
     val logSrc = Source.fromFile(log)
     try {
       val logLines = logSrc.mkString
+
+      println(s"-->$logLines<--")
 
       // Test inclusion of a header
       val regexHeader = "^namespace\\tfunctionality\\tplatform\\ttest_name\\texit_code\\tduration\\tresult".r
       assert(regexHeader.findFirstIn(logLines).isDefined, s"\nRegex: ${regexHeader.toString}; text: \n$logLines")
 
-      // Test all 'normal' steps for components
-      for (component ← components) {
+      for ((component, steps) ← components) {
         for ((step, resultPattern) ← steps) {
-          val regex = s"""testns\\t$component\\tnative\\t$step$resultPattern""".r
-          assert(regex.findFirstIn(logLines).isDefined, s"\nRegex: '${regex.toString}'; text: \n$logLines")
+          // tsv doesn't output the "start" step, so ignore that
+          if (step != "start") {
+            val regex = s"""testns\\t$component\\tnative\\t$step$resultPattern""".r
+            assert(regex.findFirstIn(logLines).isDefined, s"\nRegex: '${regex.toString}'; text: \n$logLines")
+          }
         }
       }
-
-      // Check for the one failing test of ns_divide
-      val regexFail = s"""testns\\tns_divide\\tnative\\ttest_div0\\.sh\\t1\\t\\d+\\tERROR""".r
-      assert(regexFail.findFirstIn(logLines).isDefined, s"\nRegex: '${regexFail.toString}'; text: \n$logLines")
     } finally {
       logSrc.close()
     }
@@ -107,18 +105,6 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
         "--src", nsPath
       ))
 
-    val components = List(
-      "ns_add",
-      "ns_subtract",
-      "ns_multiply",
-      "ns_divide"
-    )
-
-    val steps = List(
-      ("build_executable","\\t0\\t\\d+\\tSUCCESS"),
-      ("test\\.sh","\\t0\\t\\d+\\tSUCCESS")
-    )
-
     val logSrc = Source.fromFile(log)
     try {
       val logLines = logSrc.mkString
@@ -129,17 +115,15 @@ class MainNSTestNativeSuite extends FunSuite with BeforeAndAfterAll {
       val regexHeader2 = "^Test header".r
       assert(regexHeader2.findFirstIn(logLines).isDefined, s"\rRegex: ${regexHeader2.toString}; text: \r$logLines")
 
-      // Test all 'normal' steps for components
-      for (component ← components) {
+      for ((component, steps) ← components) {
         for ((step, resultPattern) ← steps) {
-          val regex = s"""testns\\t$component\\tnative\\t$step$resultPattern""".r
-          assert(regex.findFirstIn(logLines).isDefined, s"\nRegex: '${regex.toString}'; text: \n$logLines")
+          // tsv doesn't output the "start" step, so ignore that
+          if (step != "start") {
+            val regex = s"""testns\\t$component\\tnative\\t$step$resultPattern""".r
+            assert(regex.findFirstIn(logLines).isDefined, s"\nRegex: '${regex.toString}'; text: \n$logLines")
+          }
         }
       }
-
-      // Check for the one failing test of ns_divide
-      val regexFail = s"""testns\\tns_divide\\tnative\\ttest_div0\\.sh\\t1\\t\\d+\\tERROR""".r
-      assert(regexFail.findFirstIn(logLines).isDefined, s"\nRegex: '${regexFail.toString}'; text: \n$logLines")
     } finally {
       logSrc.close()
     }
