252f7e997b199c590630971ed811853ea3c7f345
Toni Verbeiren <toni.verbeiren@gmail.com>
2021-05-15 23:48:02 +0200

    Improve project_update

--- src/viash/project_update/script.sh
+++ src/viash/project_update/script.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 # get the root of the repository
-REPO_ROOT=$par_pwd
+REPO_ROOT=`pwd`
 
-# ensure that the command below is run from the root of the repository
-cd "$REPO_ROOT"
+if [ ! -d "$par_bin" ]; then
+  echo "> Creating $par_bin"
+  mkdir "$par_bin"
+fi
 
 # remove previous binaries
 echo "> Cleanup"
-rm bin/viash*
-rm bin/project_*
-rm bin/skeleton*
+if [ -f $par_bin/viash* ]; then
+  echo "  > Removing previous versions of viash"
+  rm $par_bin/viash*
+fi
+if [ -f $par_bin/project_update ]; then
+  echo "  > Removing previous versions of project binaries"
+  rm $par_bin/project_*
+fi
+if [ -f $par_bin/skeleton ]; then
+  echo "  > Removing previous versions of skeleton binary"
+  rm $par_bin/skeleton*
+fi
 
 # build helper components
 build_dir=$(mktemp -d)
@@ -27,7 +38,7 @@ fetch --repo="https://github.com/data-intuitive/viash" --branch="develop" --sour
 echo "> Building components"
 viash ns build \
   -s "$build_dir" \
-  -t bin \
+  -t "$par_bin" \
   --flatten \
   -c '.functionality.arguments[.name == "--registry"].default := "'$par_registry'"' \
   -c '.functionality.arguments[.name == "--viash"].default := "'$par_viash'"' \
@@ -35,7 +46,7 @@ viash ns build \
   -c '.functionality.arguments[.name == "--namespace_separator"].default := "'$par_namespace_separator'"'
 
 # copy viash itself
-echo "> Install viash under bin/"
-cp `which viash` $par_bin
+echo "> Install viash under $par_bin"
+cp `which viash` "$par_bin"
 
 echo "> Done, happy viash-ing!"
