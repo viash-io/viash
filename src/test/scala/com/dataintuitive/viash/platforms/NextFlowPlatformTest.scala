ab21f9dde45d31655722aecdb008a015e37fd0fa
Hendrik Cannoodt <hendrik@griffintech.be>
2021-07-02 18:25:06 +0200

    Use temporary folder for the nextflow testbench
    nextflow creates a whole bunch of files that would litter the sources, so use temporary folder

--- src/test/scala/com/dataintuitive/viash/platforms/NextFlowPlatformTest.scala
+++ src/test/scala/com/dataintuitive/viash/platforms/NextFlowPlatformTest.scala
@@ -1,21 +1,34 @@
 package com.dataintuitive.viash.platforms
 
+import com.dataintuitive.viash.helpers.IO
 import com.dataintuitive.viash.{DockerTest, NextFlowTest, TestHelper}
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 import java.io.File
-import java.nio.file.Paths
+import java.nio.file.{Files, Path, Paths}
 import scala.io.Source
 
+import java.io.IOException
+import java.io.UncheckedIOException
 
-class NextFlowPlatformTest extends FunSuite {
+class NextFlowPlatformTest extends FunSuite with BeforeAndAfterAll {
+  // temporary folder to work in
+  private val temporaryFolder = IO.makeTemp("viash_tester_nextflow")
+  private val tempFolStr = temporaryFolder.toString
+
   // path to namespace components
   private val rootPath = getClass.getResource("/testnextflow/").getPath
-  private val srcPath = Paths.get(rootPath, "src").toFile.toString
-  private val targetPath = Paths.get(rootPath, "target").toFile.toString
+  private val srcPath = Paths.get(tempFolStr, "src").toFile.toString
+  private val targetPath = Paths.get(tempFolStr, "target").toFile.toString
 
   // convert testbash
   test("build pipeline components", DockerTest, NextFlowTest) {
+
+    // copy resources to temporary folder so we can build in a clean environment
+    for (resource <- List("src", "workflows", "resources"))
+      copyFolder(Paths.get(rootPath, resource).toString, Paths.get(tempFolStr, resource).toString)
+
+    // build the nextflow containers
     val (_, _) = TestHelper.testMainWithStdErr(
       "ns", "build",
       "-s", srcPath,
@@ -26,7 +39,7 @@ class NextFlowPlatformTest extends FunSuite {
     import sys.process._
     val output = Process(
       Seq("nextflow", "run", ".", "-main-script", "workflows/main.nf", "--input", "resources/*", "--publishDir", "output"),
-      new File(rootPath),
+      new File(tempFolStr),
       "NXF_VER" -> "21.04.1"
     ).!!
     val lines = output.split("\n").find(_.contains("DEBUG7"))
@@ -40,5 +53,39 @@ class NextFlowPlatformTest extends FunSuite {
 
     // TODO: check other debug flags as well.
     // TODO: change step3 into something more interesting.
+  }
+
+
+
+  // code based on https://stackoverflow.com/questions/29076439/java-8-copy-directory-recursively/34254130#34254130
+  def copyFolder(src: String, dest: String): Unit = {
+    try {
+      val stream = Files.walk(Paths.get(src))
+      try stream.forEachOrdered((sourcePath: Path) => {
+
+        def foo(sourcePath: Path) =
+          try {
+            val newPath = Paths.get(dest).resolve(Paths.get(src).relativize(sourcePath))
+            if (sourcePath.toFile.isFile) {
+              Files.copy(sourcePath, newPath)
+            }
+            else if (sourcePath.toFile.isDirectory) {
+              newPath.toFile.mkdir()
+            }
+
+          } catch {
+            case e: IOException =>
+              throw new UncheckedIOException(e)
+        }
+
+        foo(sourcePath)
+      })
+      finally if (stream != null) stream.close()
+    }
+  }
+
+
+  override def afterAll() {
+    IO.deleteRecursively(temporaryFolder)
   }
 }
