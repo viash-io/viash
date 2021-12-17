7f98f9f0f89934260078a21ec37611fa42674b1d
Robrecht Cannoodt <rcannood@gmail.com>
2021-12-17 10:00:56 +0100

    switch to curl, allow detecting latest

--- src/viash/viash_install/script.sh
+++ src/viash/viash_install/script.sh
@@ -4,8 +4,8 @@ if ! command -v unzip &> /dev/null; then
     echo "unzip needs to be installed"
     exit
 fi
-if ! command -v wget &> /dev/null; then
-    echo "wget needs to be installed"
+if ! command -v curl &> /dev/null; then
+    echo "curl needs to be installed"
     exit
 fi
 
@@ -18,21 +18,27 @@ if [ ! -d "$par_bin" ]; then
   mkdir "$par_bin"
 fi
 
+if [ "$par_tag" == "latest" ]; then
+  LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/viash-io/viash/releases/latest)
+  par_tag=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+  echo "> Detected latest version as $par_tag"
+fi
+
 echo "> Using tag $par_tag"
 
 # remove previous binaries
 echo "> Cleanup"
 if [ -f "$par_bin/viash" ]; then
-  echo "  > Removing previous versions of Viash and recent project binaries"
-  rm "$par_bin/viash*"
+  echo "> Removing previous versions of Viash and recent project binaries"
+  rm "$par_bin/"viash*
 fi
 if [ -f "$par_bin/project_update" ]; then
   echo "  > Removing previous versions of project binaries"
-  rm "$par_bin/project_*"
+  rm "$par_bin/"project_*
 fi
 if [ -f "$par_bin/skeleton" ]; then
   echo "  > Removing previous versions of skeleton binary"
-  rm "$par_bin/skeleton"
+  rm "$par_bin/"skeleton
 fi
 
 
@@ -45,15 +51,14 @@ trap clean_up EXIT
 
 
 if [ $par_tag == "develop" ]; then
-
   if ! command -v sbt &> /dev/null; then
-      echo "sbt needs to be installed"
+      echo "sbt needs to be installed to build from source"
       exit
   fi
 
   # Download Viash helper scripts
-  echo "> Downloading source (version $par_tag)"
-  wget -nv "https://github.com/viash-io/viash/archive/refs/heads/$par_tag.zip" -O "$build_dir/$par_tag.zip"
+  echo "> Downloading source v$par_tag"
+  curl -L -s "https://github.com/viash-io/viash/archive/refs/heads/$par_tag.zip" -o "$build_dir/$par_tag.zip"
   unzip -q "$build_dir/$par_tag.zip" -d "$build_dir"
 
   # Build Viash
@@ -61,29 +66,30 @@ if [ $par_tag == "develop" ]; then
   cd "$build_dir/viash-$par_tag"
   ./configure
   make bin/viash
-  cp bin/viash "$par_bin"
   cd "$REPO_ROOT"
+  cp "$build_dir/viash-$par_tag/bin/viash" "$par_bin"
 else
   # Download Viash
   echo "> Downloading Viash v$par_tag under $par_bin"
-  wget -nv "https://github.com/viash-io/viash/releases/download/$par_tag/viash" -O "$par_bin/viash"
+  curl -L -s "https://github.com/viash-io/viash/releases/download/$par_tag/viash" -o "$par_bin/viash"
   chmod +x "$par_bin/viash"
 
   # Download Viash helper scripts
   echo "> Downloading source v$par_tag"
-  wget -nv "https://github.com/viash-io/viash/archive/refs/tags/$par_tag.zip" -O "$build_dir/$par_tag.zip"
+  curl -L -s "https://github.com/viash-io/viash/archive/refs/tags/$par_tag.zip" -o "$build_dir/$par_tag.zip"
   unzip -q "$build_dir/$par_tag.zip" -d "$build_dir"
 fi
 
 # build components
-echo "> Building Viash helper script"
+echo "> Building Viash helper scripts from source"
 "$par_bin/viash" ns build \
   -s "$build_dir/viash-$par_tag/src/viash" \
   -t "$par_bin/" \
   --flatten \
-  -c '.functionality.arguments[.name == "--registry"].default := "'$par_registry'"' \
-  -c '.functionality.arguments[.name == "--viash"].default := "'$par_viash'"' \
-  -c '.functionality.arguments[.name == "--log" && root.functionality.name == "viash_test"].default := "'$par_log'"' \
-  -c '.functionality.arguments[.name == "--namespace_separator"].default := "'$par_namespace_separator'"'
+  -c ".functionality.arguments[.name == '--registry'].default := '$par_registry'" \
+  -c ".functionality.arguments[.name == '--viash'].default := '$par_viash'" \
+  -c ".functionality.arguments[.name == '--log' && root.functionality.name == 'viash_test'].default := '$par_log'" \
+  -c ".functionality.arguments[.name == '--namespace_separator'].default := '$par_namespace_separator'" \
+  -c ".functionality.version := '$par_tag'"
 
 echo "> Done, happy viash-ing!"
