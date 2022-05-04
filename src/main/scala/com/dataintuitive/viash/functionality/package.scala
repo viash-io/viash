05456f534ef4ab7d6f6d1d907bf41b836216986b
Robrecht Cannoodt <rcannood@gmail.com>
2022-05-04 13:19:11 +0200

    remove functiontype class

--- src/main/scala/com/dataintuitive/viash/functionality/package.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/package.scala
@@ -28,24 +28,6 @@ package object functionality {
   import functionality.resources._
   import com.dataintuitive.viash.helpers.Circe._
 
-  // encoder and decoder for functiontype
-  implicit val encodeFunctionType: Encoder[FunctionType] = Encoder.instance {
-    ft => Json.fromString(ft.toString.toLowerCase())
-  }
-  implicit val decodeFunctionType: Decoder[FunctionType] = Decoder.instance {
-    cursor =>
-      cursor.value.as[String].map(s =>
-        s.toLowerCase() match {
-          case "asis" => AsIs
-          case "transform" => Convert
-          case "convert" => Convert
-          case "todir" => ToDir
-          case "join" => Join
-        }
-      )
-  }
-
-
   // encoder and decoder for Functionality
   implicit val encodeFunctionality: Encoder.AsObject[Functionality] = deriveConfiguredEncoder
 
