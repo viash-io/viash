feba32698491237c24a201add02bfb61330243ab
Robrecht Cannoodt <rcannood@gmail.com>
2022-05-18 10:22:04 +0200

    refactor into match

--- src/main/scala/com/dataintuitive/viash/functionality/resources/Script.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/resources/Script.scala
@@ -39,12 +39,13 @@ trait Script extends Resource {
 
       val li =
         if (startIndex >= 0 && endIndex >= 0) {
-          val startDelimiter = raw"^\s+".r.findFirstIn(lines(startIndex))
-          val viashLinesWDelimiter = if (startDelimiter.isDefined) {
-            viashLines.flatMap(_.split("\n")).map(startDelimiter.get + _)
-          } else {
-            viashLines
-          }
+          val Whitespace = raw"^(\s+).*".r
+          val viashLinesWDelimiter = 
+            lines(startIndex) match {
+              case Whitespace(prefix) => 
+                viashLines.flatMap(_.split("\n")).map(prefix + _)
+              case _ => viashLines
+            }
 
           lines.slice(0, startIndex + 1) ++ viashLinesWDelimiter ++ lines.slice(endIndex, lines.length)
         } else {
