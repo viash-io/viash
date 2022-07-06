2843eb766811e0eb66a0015953bda12d1447a4df
Robrecht Cannoodt <rcannood@gmail.com>
2022-07-06 04:38:37 +0200

    implement yamlblob tester

--- src/test/resources/testnextflowvdsl3/workflows/pipeline3/run.sh
+++ src/test/resources/testnextflowvdsl3/workflows/pipeline3/run.sh
@@ -11,6 +11,21 @@ nextflow \
   --real_number 10.5 \
   --whole_number 10 \
   --str foo \
+  --multiple a:b:c:d \
+  --publishDir "output" \
+  -resume \
+  -entry base
+
+fooArgs="{id: foo, input: resources/lines3.txt, whole_number: 3, optional_with_default: foo, multiple: [a, b, c]}"
+barArgs="{id: bar, input: resources/lines5.txt, real_number: 0.5, optional: bar}"
+nextflow \
+  -q \
+  run . \
+  -main-script workflows/pipeline3/main.nf \
+  --param_list "[$fooArgs, $barArgs]" \
+  --real_number 10.5 \
+  --whole_number 10 \
+  --str foo \
   --publishDir "output" \
   -resume \
   -entry base
