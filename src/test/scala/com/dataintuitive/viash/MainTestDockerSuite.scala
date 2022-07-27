f4d5d9dde8a8e5478f6f60d79b491126798f6170
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-07-27 17:18:55 +0200

    derive variant config files for tests instead of committing all kinds of variants
    Requires yq for the config file manipulations

--- src/test/scala/com/dataintuitive/viash/MainTestDockerSuite.scala
+++ src/test/scala/com/dataintuitive/viash/MainTestDockerSuite.scala
@@ -2,25 +2,21 @@ package io.viash
 
 import java.nio.file.{Files, Paths, StandardCopyOption}
 
-import io.viash.helpers.IO
+import io.viash.helpers._
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 import scala.reflect.io.Directory
+import sys.process._
 
 class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   // default yaml
   private val configFile = getClass.getResource("/testbash/config.vsh.yaml").getPath
 
-  // workaround for having no '.vsh.' in the filename as to bypass detection of namespace command
-  private val configNoTestFile = getClass.getResource("/testbash/config_no_tests.vsh.yaml").getPath
-  private val configFailedTestFile = getClass.getResource("/testbash/config_failed_test.vsh.yaml").getPath
-  private val configFailedBuildFile = getClass.getResource("/testbash/config_failed_build.vsh.yaml").getPath
-  private val configNonexistentTestFile = getClass.getResource("/testbash/config_nonexistent_test.vsh.yaml").getPath
-  private val configWithSpacesFile = getClass.getResource("/testbash/config test.vsh.yaml").getPath.replaceAll("%20", " ")
-  private val configLegacyTestFile = getClass.getResource("/testbash/config_legacy.vsh.yaml").getPath
+  private val temporaryFolder = IO.makeTemp(s"viash_${this.getClass.getName}_")
+  private val tempFolStr = temporaryFolder.toString
 
-  private val configMissingFunctionalityFile = getClass.getResource("/testbash/invalid_configs/config_missing_functionality.vsh.yaml").getPath
-  private val configTextFile = getClass.getResource("/testbash/invalid_configs/config.txt").getPath
+  // private val configMissingFunctionalityFile = getClass.getResource("/testbash/invalid_configs/config_missing_functionality.vsh.yaml").getPath
+  // private val configTextFile = getClass.getResource("/testbash/invalid_configs/config.txt").getPath
   private val configInvalidYamlFile = getClass.getResource("/testbash/invalid_configs/config_invalid_yaml.vsh.yaml").getPath
 
   // custom platform yamls
@@ -70,11 +66,31 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
     checkTempDirAndRemove(testText, false)
   }
 
+  test("Prepare tests with derived configs, copy resources to temporary folder", DockerTest) {
+    val rootPath = getClass.getResource(s"/testbash/").getPath
+    TestHelper.copyFolder(rootPath, tempFolStr)
+    
+    val newConfigFilePath = deriveNewConfigWithYq(".", "default_config")
+
+    val testText = TestHelper.testMain(
+      "test",
+      "-p", "docker",
+      newConfigFilePath
+    )
+
+    assert(testText.contains("Running tests in temporary directory: "))
+    assert(testText.contains("SUCCESS! All 2 out of 2 test scripts succeeded!"))
+    assert(testText.contains("Cleaning up temporary directory"))
+
+    checkTempDirAndRemove(testText, false)
+  }
+
   test("Check test output when no tests are specified in the functionality file", NativeTest) {
+    val newConfigFilePath = deriveNewConfigWithYq("del(.functionality.test_resources)", "no_tests")
     val testText = TestHelper.testMain(
       "test",
       "-p", "native",
-      configNoTestFile
+      newConfigFilePath
     )
 
     assert(testText.contains("Running tests in temporary directory: "))
@@ -85,22 +101,26 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Check test output when a test fails", NativeTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".functionality.test_resources[0].path="tests/fail_failed_test.sh"""", "failed_test")
     val testText = TestHelper.testMainException[RuntimeException](
       "test",
       "-p", "native",
-      configFailedTestFile
+      newConfigFilePath
     )
 
     assert(testText.contains("Running tests in temporary directory: "))
-    assert(testText.contains("ERROR! Only 0 out of 1 test scripts succeeded!"))
+    assert(testText.contains("ERROR! Only 1 out of 2 test scripts succeeded!"))
     assert(!testText.contains("Cleaning up temporary directory"))
 
     checkTempDirAndRemove(testText, true)
   }
 
   test("Check failing build", DockerTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".platforms[1].apt.packages[0]="get_the_machine_that_goes_ping"""", "failed_build")
     val testOutput = TestHelper.testMainException2[RuntimeException](
-      "test", configFailedBuildFile
+      "test",
+      "-p", "docker",
+      newConfigFilePath
     )
 
     assert(testOutput.exceptionText == "Setup failed!")
@@ -113,24 +133,29 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Check test output when test doesn't exist", DockerTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".functionality.test_resources[0].path="tests/nonexistent_test.sh"""", "nonexisting_test")
     val testOutput = TestHelper.testMainException2[RuntimeException](
-      "test", configNonexistentTestFile
+      "test",
+      "-p", "docker",
+      newConfigFilePath
     )
 
-    assert(testOutput.exceptionText == "Only 0 out of 1 test scripts succeeded!")
+    assert(testOutput.exceptionText == "Only 1 out of 2 test scripts succeeded!")
 
     assert(testOutput.output.contains("Running tests in temporary directory: "))
-    assert(testOutput.output.contains("ERROR! Only 0 out of 1 test scripts succeeded!"))
+    assert(testOutput.output.contains("ERROR! Only 1 out of 2 test scripts succeeded!"))
     assert(!testOutput.output.contains("Cleaning up temporary directory"))
 
     checkTempDirAndRemove(testOutput.output, true)
   }
 
   test("Check config and resource files with spaces in the filename", DockerTest) {
+    val newConfigFilePath = Paths.get(tempFolStr, "config with spaces.vsh.yaml")
+    Files.copy(Paths.get(configFile), newConfigFilePath)
     val testText = TestHelper.testMain(
       "test",
       "-p", "docker",
-      configWithSpacesFile
+      newConfigFilePath.toString()
     )
 
     assert(testText.contains("Running tests in temporary directory: "))
@@ -141,10 +166,11 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Check standard test output with legacy 'tests' definition", DockerTest) {
+    val newConfigFilePath = deriveNewConfigWithYq("""with(.functionality ; .tests=.test_resources | del(.test_resources))""", "legacy")
     val (stdout, stderr) = TestHelper.testMainWithStdErr(
       "test",
       "-p", "docker",
-      configLegacyTestFile
+      newConfigFilePath
     )
 
     assert(stderr.contains("Notice: functionality.tests is deprecated. Please use functionality.test_resources instead."))
@@ -158,10 +184,11 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   //</editor-fold>
   //<editor-fold desc="Invalid config files">
   test("Check config file without 'functionality' specified", DockerTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".unctionality=.functionality | del(.functionality)""", "missing_functionality")
     val testOutput = TestHelper.testMainException2[RuntimeException](
       "test",
       "-p", "docker",
-      configMissingFunctionalityFile
+      newConfigFilePath
     )
 
     assert(testOutput.exceptionText.contains("must be a yaml file containing a viash config."))
@@ -169,10 +196,12 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Check valid viash config yaml but with wrong file extension") {
+    val newConfigFilePath = Paths.get(tempFolStr, "config.txt")
+    Files.copy(Paths.get(configFile), newConfigFilePath)
     val testOutput = TestHelper.testMainException2[RuntimeException](
       "test",
       "-p", "docker",
-      configTextFile
+      newConfigFilePath.toString()
     )
 
     assert(testOutput.exceptionText.contains("must be a yaml file containing a viash config."))
@@ -222,34 +251,36 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Check test output when a test fails and --keep true is specified", NativeTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".functionality.test_resources[0].path="tests/fail_failed_test.sh"""", "failed_test_keep_true")
     val testOutput = TestHelper.testMainException2[RuntimeException](
       "test",
       "-p", "native",
       "-k", "true",
-      configFailedTestFile
+      newConfigFilePath
     )
 
-    assert(testOutput.exceptionText == "Only 0 out of 1 test scripts succeeded!")
+    assert(testOutput.exceptionText == "Only 1 out of 2 test scripts succeeded!")
 
     assert(testOutput.output.contains("Running tests in temporary directory: "))
-    assert(testOutput.output.contains("ERROR! Only 0 out of 1 test scripts succeeded!"))
+    assert(testOutput.output.contains("ERROR! Only 1 out of 2 test scripts succeeded!"))
     assert(!testOutput.output.contains("Cleaning up temporary directory"))
 
     checkTempDirAndRemove(testOutput.output, true)
   }
 
   test("Check test output when a test fails and --keep false is specified", NativeTest) {
+    val newConfigFilePath = deriveNewConfigWithYq(""".functionality.test_resources[0].path="tests/fail_failed_test.sh"""", "failed_test_keep_false")
     val testOutput = TestHelper.testMainException2[RuntimeException](
       "test",
       "-p", "native",
       "-k", "false",
-      configFailedTestFile
+      newConfigFilePath
     )
 
-    assert(testOutput.exceptionText == "Only 0 out of 1 test scripts succeeded!")
+    assert(testOutput.exceptionText == "Only 1 out of 2 test scripts succeeded!")
 
     assert(testOutput.output.contains("Running tests in temporary directory: "))
-    assert(testOutput.output.contains("ERROR! Only 0 out of 1 test scripts succeeded!"))
+    assert(testOutput.output.contains("ERROR! Only 1 out of 2 test scripts succeeded!"))
     assert(testOutput.output.contains("Cleaning up temporary directory"))
 
     checkTempDirAndRemove(testOutput.output, false)
@@ -282,6 +313,30 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
   //</editor-fold>
 
   /**
+    * Derive a config file from the default config
+    * @param cmd Command to pass to yq
+    * @param name name of .sh and .vsh.yaml files
+    * @return Path to the new config file as string
+    */
+  def deriveNewConfigWithYq(cmd: String, name: String): String = {
+    val shFileStr = s"$name.sh"
+    val yamlFileStr = s"$name.vsh.yaml"
+    val newConfigFilePath = Paths.get(tempFolStr, s"$yamlFileStr")
+    Files.copy(Paths.get(configFile), newConfigFilePath)
+
+    val fullCmd = s"""yq '$cmd' -i ./$yamlFileStr"""
+    val newConfigShPath = Paths.get(tempFolStr, shFileStr)
+    Files.writeString(newConfigShPath, fullCmd)
+
+    Exec.run(
+      Seq("sh", shFileStr),
+      cwd = Some(Paths.get(tempFolStr).toFile())
+    )
+
+    newConfigFilePath.toString
+  }
+
+  /**
    * Searches the output generated by Main.main() during tests for the temporary directory name and verifies if it still exists or not.
    * If directory was expected to be present and actually is present, it will be removed.
    * @param testText the text generated by Main.main()
@@ -312,5 +367,9 @@ class MainTestDockerSuite extends FunSuite with BeforeAndAfterAll {
 
     // folder should always have been removed at this stage
     assert(!tempFolder.exists)
+  }
+
+  override def afterAll() {
+    IO.deleteRecursively(temporaryFolder)
   }
 }
