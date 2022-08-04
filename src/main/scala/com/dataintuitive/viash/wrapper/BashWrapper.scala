fb181a7ef2f8a218cfae3dd76f8386a804eb9d21
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-08-04 12:15:17 +0200

    Check argument is already declared when parsing next argument
    Throw error when redefining an argument unless --multiple is specified

--- src/main/scala/com/dataintuitive/viash/wrapper/BashWrapper.scala
+++ src/main/scala/com/dataintuitive/viash/wrapper/BashWrapper.scala
@@ -42,7 +42,10 @@ object BashWrapper {
          |  $env="$$$env${multiple_sep.get}"$value
          |fi""".stripMargin.split("\n")
     } else {
-      Array(env + "=" + value)
+      Array(
+        s"""[ -n "$$$env" ] && ViashError Bad arguments for option '$env': \\'$$$env $$2\\' - you should provide exactly one argument for this option. && exit 1""",
+        env + "=" + value
+      )
     }
   }
 
