4fce48358c234795e65cbcdd458e56d710a13350
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2022-08-05 15:51:31 +0200

    Fix newline

--- src/main/scala/com/dataintuitive/viash/functionality/arguments/IntegerArgument.scala
+++ src/main/scala/com/dataintuitive/viash/functionality/arguments/IntegerArgument.scala
@@ -34,7 +34,8 @@ import io.viash.helpers._
                |    alternatives: ["-c"]
                |```  
                |  
-""".stripMargin)case class IntegerArgument(
+""".stripMargin)
+case class IntegerArgument(
   @description("""
                  |The name of the argument. Can be in the formats `--foo`, `-f` or `foo`. The number of dashes determines how values can be passed:  
                  |
