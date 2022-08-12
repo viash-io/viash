4b75174310ecb7a6b3feeb9b814e434d9113bf78
Robrecht Cannoodt <rcannood@gmail.com>
2022-08-12 11:33:37 +0200

    fix test

--- src/test/scala/com/dataintuitive/viash/MainBuildDockerSuite.scala
+++ src/test/scala/com/dataintuitive/viash/MainBuildDockerSuite.scala
@@ -87,7 +87,9 @@ class MainBuildDockerSuite extends FunSuite with BeforeAndAfterAll {
         "--optional_with_default", "bar",
         "--multiple", "foo",
         "--multiple=bar",
-        "d", "e", "f"
+        "d", "e", "f",
+        "---n_proc", "2",
+        "---memory", "1gb"
       )
     )
 
@@ -132,7 +134,9 @@ class MainBuildDockerSuite extends FunSuite with BeforeAndAfterAll {
           executable.toString,
           "--real_number", "123.456",
           "--whole_number", "789",
-          "-s", "my$weird#string"
+          "-s", "my$weird#string",
+          "---n_proc", "", // unset defaults
+          "---memory", "" // unset defaults
         )
       )
 
