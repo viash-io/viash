ec5c48d9e8b6d60c0f0ec652c9c07fac0c2d49a0
Robrecht Cannoodt <rcannood@gmail.com>
2022-09-05 15:53:12 +0200

    fix tests

--- src/test/scala/io/viash/MainNSExecNativeSuite.scala
+++ src/test/scala/io/viash/MainNSExecNativeSuite.scala
@@ -19,34 +19,39 @@ class MainNSExecNativeSuite extends FunSuite with BeforeAndAfterAll {
     "ns_power",
   )
 
-
   private val temporaryFolder = IO.makeTemp("viash_ns_exec")
   private val tempFolStr = temporaryFolder.toString
 
   test("Check whether ns exec \\; works") {
-    val (stdout, stderr) = TestHelper.testMainWithStdErr(
-      "ns", "exec",
-      "--src", nsPath,
-      "echo {dir} {path} \\;"
-    )
+    val (stdoutRaw, stderr) =
+      TestHelper.testMainWithStdErr(
+        "ns", "exec",
+        "--src", nsPath,
+        "echo _{functionality-name}_ -{dir}- !{path}! \\;"
+      )
+    val stdout = stdoutRaw.replaceAll(nsPath, "src/")
 
     for (component ← components) {
-      val regexCommand = s"""\\+ echo .*src/test/resources/testns/src/$component/? .*src/test/resources/testns/src/$component/config.vsh.yaml""".r
+      val regexCommand = s"""\\+ echo _${component}_ -src/$component/?- !src/$component/config.vsh.yaml!""".r
       assert(regexCommand.findFirstIn(stdout).isDefined, s"\nRegex: $regexCommand; text: \n$stdout")
-      val outputCommand = s"""  Output: .*src/test/resources/testns/src/$component/? .*src/test/resources/testns/src/$component/config.vsh.yaml""".r
+      val outputCommand = s"""  Output: _${component}_ -src/$component/?- !src/$component/config.vsh.yaml!""".r
       assert(outputCommand.findFirstIn(stdout).isDefined, s"\nRegex: $outputCommand; text: \n$stdout")
     }
   }
 
   test("Check whether ns exec + works") {
-    val (stdout, stderr) = TestHelper.testMainWithStdErr(
+    val (stdoutRaw, stderr) = TestHelper.testMainWithStdErr(
       "ns", "exec",
       "--src", nsPath,
       "echo {path} +"
     )
-    val regexCommand = s"""\\+ echo .*src/test/resources.*src/test/resources.*src/test/resources.*src/test/resources""".r // can't guarantee order of components
+    val stdout = stdoutRaw.replaceAll(nsPath, "src/")
+    println(stdout)
+
+    // can't guarantee order of components
+    val regexCommand = s"""\\+ echo src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml""".r
     assert(regexCommand.findFirstIn(stdout).isDefined, s"\nRegex: $regexCommand; text: \n$stdout")
-    val outputCommand = s"""  Output: .*src/test/resources.*src/test/resources.*src/test/resources.*src/test/resources""".r
+    val outputCommand = s"""  Output: src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml src/[^/]*/config.vsh.yaml""".r
     assert(outputCommand.findFirstIn(stdout).isDefined, s"\nRegex: $outputCommand; text: \n$stdout")
   }
 
