6d6c4f7653485753df429abe938c394bd06983ca
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-10-17 15:00:54 +0200

    Map functionality.dependency.repository when using names to functionality.repositories
    Programmed in functional way, could probably benefit from using lenses

--- src/main/scala/io/viash/helpers/DependencyResolver.scala
+++ src/main/scala/io/viash/helpers/DependencyResolver.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020  Data Intuitive
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.viash.helpers
 
 import java.nio.file.{ Path, Paths }
@@ -32,14 +49,14 @@ object DependencyResolver {
 
     // Check all fun.repositories have valid names
     val repositories = config.functionality.repositories
-    require(repositories.groupBy(r => r.name).map{ case(k, l) => l.length }.max == 1, "Repository names should be unique")
+    require(repositories.isEmpty || repositories.groupBy(r => r.name).map{ case(k, l) => l.length }.max == 1, "Repository names should be unique")
     require(repositories.filter(r => r.name.isEmpty()).length == 0, "Repository names can't be empty")
 
 
     // Convert all fun.dependency.repository with sugar syntax to full repositories
     // val repoRegex = raw"(\w+)://([A-Za-z0-9/_\-\.]+)@([A-Za-z0-9]+)".r  // TODO improve regex
     val repoRegex = raw"(\w+://[A-Za-z0-9/_\-\.]+@[A-Za-z0-9]*)".r
-    config.copy(functionality = config.functionality.copy(
+    val config1 = config.copy(functionality = config.functionality.copy(
       dependencies = config.functionality.dependencies.map(d => 
         d.repository match {
           case Left(repoRegex(s)) => d.copy(repository = Right(Repository.fromSugarSyntax(s)))
@@ -49,22 +66,22 @@ object DependencyResolver {
     )))
 
     // Check all remaining fun.dependency.repository names (Left) refering to fun.repositories can be matched
+    val dependencyRepoNames = config1.functionality.dependencies.flatMap(_.repository.left.toOption)
+    val definedRepoNames = config1.functionality.repositories.map(_.name)
+    dependencyRepoNames.foreach(name =>
+      require(definedRepoNames.contains(name), s"Named dependency repositories should exist in the list of repositories. '$name' not found.")
+      )
 
-    // Move repositories defined in dependencies to the list of repositories, link by string name
-
-
-    // OLD code, works in the wrong direction
-    // map the repository string to the repository class => Either.Right -> Either.Left
-    // val config1 = config.copy(
-    //   functionality = config.functionality.copy(
-    //     dependencies = config.functionality.dependencies.map(d => 
-    //       d.repository match {
-    //         case Left(r) => d
-    //         case Right(s) => d.copy(repository = Left(repositories.find(_.name == s).get)) // TODO if name not found, this blows up
-    //       }
-    //   )))
-
-    // 
+    // Match repositories defined in dependencies by name to the list of repositories, fill in repository in dependency
+    val config2 = config1.copy(
+      functionality = config1.functionality.copy(
+        dependencies = config1.functionality.dependencies.map(d => 
+          d.repository match {
+            case Left(name) => d.copy(repository = Right(config1.functionality.repositories.find(r => r.name == name).get))
+            case _ => d
+          }
+        )
+    ))
 
     // val actualRepo = 
     //   rawRepo match {
@@ -77,12 +94,14 @@ object DependencyResolver {
     //   }
 
     // get caches and store in repository classes
-    config.copy(
+    config2.copy(
       // provide local cache for all repositories
-      functionality = config.functionality.copy(
-        repositories = config.functionality.repositories.map{ repo =>
+      functionality = config2.functionality.copy(
+        dependencies = config2.functionality.dependencies.map{ d =>
+          // This should always be Either Right (Repository)
+          val repo = d.repository.right.get
           val localRepoPath = cacheRepo(repo)
-          repo.copyRepo(localPath = localRepoPath.toString)
+          d.copy(repository = Right(repo.copyRepo(localPath = localRepoPath.toString)))
         })
       )
   }
