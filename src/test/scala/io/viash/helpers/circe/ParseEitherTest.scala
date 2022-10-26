210f7c590bd67ea9d4b3857d69d8e286ef13cb7c
Robrecht Cannoodt <rcannood@gmail.com>
2022-10-26 02:03:50 +0200

    Add ParseEitherTest

---
+++ src/test/scala/io/viash/helpers/circe/ParseEitherTest.scala
@@ -0,0 +1,30 @@
+package io.viash.helpers.circe
+
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import io.circe._
+import io.circe.yaml.parser
+import io.circe.generic.extras.semiauto.{deriveConfiguredDecoder, deriveConfiguredEncoder}
+
+class ParseEitherTest extends FunSuite with BeforeAndAfterAll {
+  case class A(foo: String)
+  case class B(bar: Int)
+  case class XXX(ab: Either[A, B])
+  implicit val encodeA: Encoder.AsObject[A] = deriveConfiguredEncoder
+  implicit val decodeA: Decoder[A] = deriveConfiguredDecoder
+  implicit val encodeB: Encoder.AsObject[B] = deriveConfiguredEncoder
+  implicit val decodeB: Decoder[B] = deriveConfiguredDecoder
+  implicit val encodeXXX: Encoder.AsObject[XXX] = deriveConfiguredEncoder
+  implicit val decodeXXX: Decoder[XXX] = deriveConfiguredDecoder
+
+  test("parsing either works with left element") {
+    val json: Json = parser.parse("ab:\n  foo: str").getOrElse(Json.Null)
+    val parsed = json.as[XXX].right.get
+    assert(parsed == XXX(ab = Left(A("str"))))
+  }
+
+  test("parsing either works with right element") {
+    val json: Json = parser.parse("ab:\n  bar: 10").getOrElse(Json.Null)
+    val parsed = json.as[XXX].right.get
+    assert(parsed == XXX(ab = Right(B(10))))
+  }
+}
