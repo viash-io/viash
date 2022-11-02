79abac24a378dabf88cce76202d1bb82e3336afb
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2022-11-02 11:30:10 +0100

    Move folder to copy to src and add extra debug prints

--- src/test/scala/io/viash/auxiliary/MainTestUnderscoreComponents.scala
+++ src/test/scala/io/viash/auxiliary/MainTestUnderscoreComponents.scala
@@ -8,24 +8,28 @@ import io.viash.config.Config
 import scala.io.Source
 import io.viash.helpers.{IO, Exec}
 import io.viash.TestHelper
-
+import scala.sys.process.{Process, ProcessLogger}
 class MainTestUnderscoreComponents extends FunSuite with BeforeAndAfterAll {
   private val nsPath = System.getProperty("user.dir") + "/src/viash"
 
   private val temporaryFolder = IO.makeTemp("viash_tester")
   private val tempFolStr = temporaryFolder.toString
 
+  private val newNsPath = Paths.get(tempFolStr, "src").toString
+
   println(s"nsPath: $nsPath")
   println(s"tempFolStr: $tempFolStr")
   
   test("Copy underscore components into temporary folder") {
-    TestHelper.copyFolder(nsPath, tempFolStr)
+    TestHelper.copyFolder(nsPath, newNsPath)
+    Process(Seq("tree", tempFolStr)).!(ProcessLogger(println, println))
+    Process(Seq("ls", "-R", tempFolStr)).!(ProcessLogger(println, println))
   }
 
   test("Test running viash ns test on the underscore components") {
     val (stdout, stderr, exitCode) = TestHelper.testMainWithStdErr(
       "ns", "test",
-      "--src", tempFolStr,
+      "--src", newNsPath,
     )
 
     assert(!stdout.contains("ERROR"), stdout)
