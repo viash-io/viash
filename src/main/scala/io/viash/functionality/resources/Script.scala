5d3f83a93411bf2655dd06dd6bbe46762d182b31
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-05-09 11:02:24 +0200

    Fix empty lines being added when running viash config inject
    Check whether the mods.header is empty to prevent adding it as an empty line.
    Added an extra check in the e2e config inject test_languages testbench
    Fixes (#377)

--- src/main/scala/io/viash/functionality/resources/Script.scala
+++ src/main/scala/io/viash/functionality/resources/Script.scala
@@ -60,7 +60,10 @@ trait Script extends Resource {
           Array(companion.commentStr + companion.commentStr + " VIASH END") ++
           lines
         }
-      val li2 = Array(mods.header) ++ li ++ Array(mods.footer)
+      val li2 = mods.header match {
+          case "" => li :+ mods.footer
+          case _ => mods.header +: li :+ mods.footer
+        }
 
       li2.mkString("\n")
     })
