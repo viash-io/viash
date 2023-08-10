70885d9a7e82c21e077b17019b3ee7e5c801dec2
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-08-10 11:00:45 +0200

    Add test variants as experiments show they highlight different edge cases

--- src/test/scala/io/viash/config_mods/DeleteTest.scala
+++ src/test/scala/io/viash/config_mods/DeleteTest.scala
@@ -31,8 +31,21 @@ class DeleteTest extends AnyFunSuite {
       |  - name: qux
       |    a: 3
       |""".stripMargin).toOption.get
+
+  test("test delete single entry from a list #1") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: baz
+        |    a: 2
+        |  - name: qux
+        |    a: 3
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a == 1])""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
   
-  test("test delete single entry from a list") {
+  test("test delete single entry from a list #2") {
     val expected1: Json = parse(
       """foo:
         |  - name: bar
@@ -45,7 +58,34 @@ class DeleteTest extends AnyFunSuite {
     assert(res1 == expected1)
   }
 
-  test("test delete field from a single list entry") {
+  test("test delete single entry from a list #3") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: bar
+        |    a: 1
+        |  - name: baz
+        |    a: 2
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a == 3])""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete field from a single list entry #1") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: bar
+        |  - name: baz
+        |    a: 2
+        |  - name: qux
+        |    a: 3
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a == 1].a)""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete field from a single list entry #2") {
     val expected1: Json = parse(
       """foo:
         |  - name: bar
@@ -59,18 +99,54 @@ class DeleteTest extends AnyFunSuite {
     assert(res1 == expected1)
   }
 
-  test("test delete multiple entries from a list") {
+  test("test delete field from a single list entry #3") {
     val expected1: Json = parse(
       """foo:
         |  - name: bar
         |    a: 1
+        |  - name: baz
+        |    a: 2
+        |  - name: qux
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a == 3].a)""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete multiple entries from a list #1") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: bar
+        |    a: 1
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 1])""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete multiple entries from a list #2") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: baz
+        |    a: 2
         |""".stripMargin).toOption.get
     val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 2])""")
     val res1 = cmd1.apply(baseJson, false)
     assert(res1 == expected1)
   }
 
-  test("test delete field from multiple list entries") {
+  test("test delete multiple entries from a list #3") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: qux
+        |    a: 3
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 3])""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete field from multiple list entries #1") {
     val expected1: Json = parse(
       """foo:
         |  - name: bar
@@ -79,6 +155,31 @@ class DeleteTest extends AnyFunSuite {
         |  - name: qux
         |""".stripMargin).toOption.get
     val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 1].a)""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+
+  test("test delete field from multiple list entries #2") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: bar
+        |  - name: baz
+        |    a: 2
+        |  - name: qux
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 2].a)""")
+    val res1 = cmd1.apply(baseJson, false)
+    assert(res1 == expected1)
+  }
+  test("test delete field from multiple list entries #3") {
+    val expected1: Json = parse(
+      """foo:
+        |  - name: bar
+        |  - name: baz
+        |  - name: qux
+        |    a: 3
+        |""".stripMargin).toOption.get
+    val cmd1 = ConfigModParser.block.parse("""del(.foo[.a != 3].a)""")
     val res1 = cmd1.apply(baseJson, false)
     assert(res1 == expected1)
   }
