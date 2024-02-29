49860b83b2980f454483bc34d29225a7784a164e
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-02-29 10:04:14 +0100

    Fix inconsistencies in the documentation for docker setup strategy

--- src/main/scala/io/viash/platforms/DockerPlatform.scala
+++ src/main/scala/io/viash/platforms/DockerPlatform.scala
@@ -125,8 +125,8 @@ case class DockerPlatform(
       +| `alwayspullelsebuild` / `pullelsebuild` |  Try to pull the image from a registry and build it if it doesn't exist.
       +| `alwayspullelsecachedbuild` / `pullelsecachedbuild` |  Try to pull the image from a registry and build it with caching if it doesn't exist.
       +| `ifneedbepull` |  If the image does not exist locally, pull the image.
-      +| `ifneedbepullelsebuild` |  If the image does not exist locally, pull the image. If the image does exist, build it.
-      +| `ifneedbepullelsecachedbuild` | If the image does not exist locally, pull the image. If the image does exist, build it with caching enabled.
+      +| `ifneedbepullelsebuild` |  If the image does not exist locally, pull the image. If the image does not exist, build it.
+      +| `ifneedbepullelsecachedbuild` | If the image does not exist locally, pull the image. If the image does not exist, build it with caching enabled.
       +| `push` | Push the container to [Docker Hub](https://hub.docker.com)  or the @[specified docker registry](docker_registry).
       +| `pushifnotpresent` | Push the container to [Docker Hub](https://hub.docker.com) or the @[specified docker registry](docker_registry) if the @[tag](docker_tag) does not exist yet.
       +| `donothing` / `meh` | Do not build or pull anything.
