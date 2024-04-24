03f3feca1e728c9dbfb76f0de76ecbc717a52787
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-04-24 09:56:22 +0200

    Add extra testbench for computationalRequirements and fix "10M" notation

--- src/main/scala/io/viash/config/ComputationalRequirements.scala
+++ src/main/scala/io/viash/config/ComputationalRequirements.scala
@@ -36,22 +36,11 @@ case class ComputationalRequirements(
 
   def memoryAsBytes: Option[BigInt] = {
     val Regex = "^([0-9]+) *([kmgtp]i?b?|b)$".r
-    val lookup = Map(
-      "b" -> (0, 1000),
-      "kb" -> (1, 1000),
-      "mb" -> (2, 1000),
-      "gb" -> (3, 1000),
-      "tb" -> (4, 1000),
-      "pb" -> (5, 1000),
-      "kib" -> (1, 1024),
-      "mib" -> (2, 1024),
-      "gib" -> (3, 1024),
-      "tib" -> (4, 1024),
-      "pib" -> (5, 1024)
-    )
+    val lookup = "bkmgtp"
     memory.map(_.toLowerCase()) match {
       case Some(Regex(amnt, unit)) => 
-        val (exp, multiplier) = lookup(unit)
+        val exp = lookup.indexOf(unit.take(1))
+        val multiplier = if (unit.contains("i")) 1024 else 1000
         Some(BigInt(amnt) * BigInt(multiplier).pow(exp))
       case Some(m) =>
         throw new RuntimeException(s"Invalid value \"$m\" as memory computational requirement.")
