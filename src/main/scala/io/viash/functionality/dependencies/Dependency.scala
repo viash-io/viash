894ec36d791dae6db57ae1806169af6c1c6d3f14
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-04-30 16:23:23 +0200

    fix the dependency.name example

--- src/main/scala/io/viash/functionality/dependencies/Dependency.scala
+++ src/main/scala/io/viash/functionality/dependencies/Dependency.scala
@@ -60,7 +60,7 @@ import io.viash.exceptions.MissingBuildYamlException
 )
 case class Dependency(
   @description("The full name of the dependency component. This should include the namespace.")
-  @example("name: \"my_namespace\"component", "yaml")
+  @example("""name: "my_namespace/component"""", "yaml")
   name: String,
 
   @description("An alternative name for the dependency component. This can include a namespace if so needed.")
