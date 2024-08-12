2b81cc95caf661f699f740c99c75ed07754404e7
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-12 17:20:11 +0200

    wip helper functions

---
+++ src/main/resources/io/viash/helpers/bashutils/ViashRenderYaml.sh
@@ -0,0 +1,82 @@
+
+
+function ViashRenderQuotedValue {
+  local key="$1"
+  local value="$2"
+  if [ "$value" == "UNDEFINED_ITEM" ]; then
+    echo "null"
+    return
+  fi
+  # escape quotes, backslashes and newlines, and then surround by quotes
+  echo "$value" | sed 's#"#\\"#g' | sed 's#\\#\\\\#g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's#^#"#g' | sed 's#$#"#g'
+}
+
+function ViashRenderBooleanValue {
+  local key="$1"
+  local value="$2"
+  if [ "$value" == "UNDEFINED_ITEM" ]; then
+    echo "null"
+    return
+  fi
+  value=$(echo "$value" | tr '[:upper:]' '[:lower:]')
+  if [[ "$value" == "true" || "$value" == "yes" ]]; then
+    echo "true"
+  elif [[ "$value" == "false" || "$value" == "no" ]]; then
+    echo "false"
+  else
+    echo "Argument '$key' has to be a boolean, but got '$value'. Use '--help' to get more information on the parameters."
+  fi
+}
+
+# can be infinite too
+function ViashRenderUnquotedValue {
+  local key="$1"
+  local value="$2"
+  if [ "$value" == "UNDEFINED_ITEM" ]; then
+    echo "null"
+    return
+  fi
+  echo "$value"
+}
+
+function ViashRenderYamlKeyValue {
+  local key="$1"
+  local type="$2"
+  local multiple="$3"
+  shift 3
+  
+  local out="$key: "
+
+  if [ "$1" == "UNDEFINED" ]; then
+    out+="null"
+    echo $out
+    return
+  fi
+
+  if [ "$multiple" == "true" ]; then
+    out+="["
+  fi
+
+  first_elem=1
+
+  for value in "$@"; do
+    if [ $first_elem -eq 1 ]; then
+      first_elem=0
+    else
+      out+=", "
+    fi
+    if [[ "$type" == "string" || "$type" == "file" ]]; then
+      out+="$(ViashRenderQuotedValue "$key" "$value")"
+    elif [[ "$type" == "boolean" || "$type" == "boolean_true" || "$type" == "boolean_false" ]]; then
+      out+="$(ViashRenderBooleanValue "$key" "$value")"
+    else
+      out+="$(ViashRenderUnquotedValue "$key" "$value")"
+    fi
+  done
+
+  if [ "$multiple" == "true" ]; then
+    out+="]"
+  fi
+
+  echo "$out"
+}
