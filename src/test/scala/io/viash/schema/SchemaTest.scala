4c8cb7de78f65eba0c3bba54e0819abaea2cfc0f
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-12-09 11:30:28 +0100

    Remove testbench for method that was removed

--- src/test/scala/io/viash/schema/SchemaTest.scala
+++ src/test/scala/io/viash/schema/SchemaTest.scala
@@ -9,27 +9,7 @@ import io.viash.helpers.Logger
 
 class SchemaTest extends AnyFunSuite with BeforeAndAfterAll with PrivateMethodTester{
   Logger.UseColorOverride.value = Some(false)
-
-  test("Check type name trimming") {
-    val checks = Map (
-      "foo" -> "foo",
-      "foo.bar" -> "bar",
-      "foo.bar.baz" -> "baz",
-      "foo.bar[baz]" -> "bar[baz]",
-      "foo[bar.baz]" -> "foo[baz]",
-      "foo[bar,baz]" -> "foo[bar,baz]",
-      "foo[bar.baz,quux]" -> "foo[baz,quux]",
-      "foo[bar,baz.quux]" -> "foo[bar,quux]"
-    )
-
-    val trimTypeName = PrivateMethod[String](Symbol("trimTypeName"))
-
-    for ((k, v) <- checks) {
-      val res = CollectedSchemas invokePrivate trimTypeName(k)
-      assert(res == v, s"$k -> $v != $res")
-    }
-  }
-    
+   
   test("All schema class val members should be annotated") {
     val nonAnnotated = CollectedSchemas.getAllNonAnnotated
 
