7c233aadbad35beaa2a1af3d06323b7f7c235b6a
Robrecht Cannoodt <rcannood@gmail.com>
2025-03-21 09:41:06 +0100

    remove comments that are only useful for the developer

--- src/main/scala/io/viash/wrapper/BashWrapper.scala
+++ src/main/scala/io/viash/wrapper/BashWrapper.scala
@@ -731,12 +731,11 @@ object BashWrapper {
     val helpStrings = 
       """Viash built in Computational Requirements:
         |    ---cpus=INT
-        |        Number of CPUs to use. This will be passed to the component via `meta["cpus"]`.
+        |        Number of CPUs to use.
         |    ---memory=STRING
-        |        Amount of memory to use. Examples: 4GB, 3MiB. This will be passed to the component
-        |        via `meta["memory_b"]` and `meta["memory_kb"]`, `meta["memory_mb"]`, and so on.
+        |        Amount of memory to use. Examples: 4GB, 3MiB.
         |    $VIASH_TEMP
-        |        The temporary directory to use. This will be passed to the component via `meta["temp_dir"]`.
+        |        The temporary directory to use.
         |""".stripMargin
 
     val compArgs = List(
