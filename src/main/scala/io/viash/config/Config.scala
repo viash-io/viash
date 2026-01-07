2ce616529f0adf76312c79d53138db83d595fcb4
Robrecht Cannoodt <rcannood@gmail.com>
2026-01-07 16:58:13 +0100

    dot is now included in the extension

--- src/main/scala/io/viash/config/Config.scala
+++ src/main/scala/io/viash/config/Config.scala
@@ -564,7 +564,7 @@ object Config extends Logging {
     val configUriStr = configUri.toString
 
     // get extension
-    val extension = configUriStr.substring(configUriStr.lastIndexOf(".") + 1).toLowerCase()
+    val extension = configUriStr.substring(configUriStr.lastIndexOf(".")).toLowerCase()
 
     // get basename
     val basenameRegex = ".*/".r
@@ -572,7 +572,7 @@ object Config extends Logging {
 
     // detect whether a script (with joined header) was passed or a joined yaml
     // using the extension
-    if ((extension == "yml" || extension == "yaml") && configStr.contains("name:")) {
+    if ((extension == ".yml" || extension == ".yaml") && configStr.contains("name:")) {
       (configStr, None)
     } else if (Script.extensions.contains(extension)) {
       // detect scripting language from extension
