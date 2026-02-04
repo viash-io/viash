5f0919d7d2c0d9f8c900d970097ac42c0d7942a6
Robrecht Cannoodt <rcannood@gmail.com>
2026-02-04 16:55:41 +0100

    fix scala parsers

--- src/main/resources/io/viash/languages/scala/ViashParseJson.scala
+++ src/main/resources/io/viash/languages/scala/ViashParseJson.scala
@@ -20,57 +20,74 @@ import java.io.File
 
 object ViashJsonParser {
   
-  def parseJson(jsonPath: Option[String] = None): Map[String, Any] = {
-    val path = jsonPath.getOrElse {
-      sys.env.getOrElse("VIASH_WORK_PARAMS", 
+  def parseJson(path: Option[String] = None): Map[String, Any] = {
+    val jsonPath = path.getOrElse {
+      sys.env.getOrElse("VIASH_WORK_PARAMS",
         throw new RuntimeException("VIASH_WORK_PARAMS environment variable not set"))
     }
     
-    val file = new File(path)
+    val file = new File(jsonPath)
     if (!file.exists()) {
-      throw new RuntimeException(s"Parameters file not found: $path")
+      throw new RuntimeException(s"Parameters file not found: $jsonPath")
     }
     
     val jsonText = Source.fromFile(file).mkString
-    parseJsonString(jsonText).asInstanceOf[Map[String, Any]]
+    parse(jsonText).asInstanceOf[Map[String, Any]]
   }
   
-  private def parseJsonString(json: String): Any = {
+  private def parse(json: String): Any = {
     var pos = 0
     
     def skipWhitespace(): Unit = {
       while (pos < json.length && json(pos).isWhitespace) pos += 1
     }
     
-    def parseValue(): Any = {
+    def peek: Char = {
       skipWhitespace()
-      if (pos >= json.length) throw new RuntimeException("Unexpected end of JSON")
-      
-      json(pos) match {
+      if (pos < json.length) json(pos) else throw new RuntimeException("Unexpected end of JSON")
+    }
+    
+    def consume(expected: Char): Unit = {
+      skipWhitespace()
+      if (pos >= json.length || json(pos) != expected) {
+        throw new RuntimeException(s"Expected '$expected' at position $pos")
+      }
+      pos += 1
+    }
+    
+    def parseValue(): Any = {
+      peek match {
         case '"' => parseString()
         case '{' => parseObject()
         case '[' => parseArray()
         case 't' | 'f' => parseBoolean()
         case 'n' => parseNull()
         case c if c == '-' || c.isDigit => parseNumber()
-        case c => throw new RuntimeException(s"Unexpected character: $c")
+        case c => throw new RuntimeException(s"Unexpected character '$c' at position $pos")
       }
     }
     
     def parseString(): String = {
-      pos += 1
+      consume('"')
       val sb = new StringBuilder
       while (pos < json.length && json(pos) != '"') {
         if (json(pos) == '\\') {
           pos += 1
-          if (pos >= json.length) throw new RuntimeException("Unterminated string")
+          if (pos >= json.length) throw new RuntimeException("Unterminated string escape")
           json(pos) match {
             case 'n' => sb.append('\n')
             case 't' => sb.append('\t')
             case 'r' => sb.append('\r')
+            case 'b' => sb.append('\b')
+            case 'f' => sb.append('\f')
             case '\\' => sb.append('\\')
             case '"' => sb.append('"')
             case '/' => sb.append('/')
+            case 'u' =>
+              if (pos + 4 >= json.length) throw new RuntimeException("Invalid unicode escape")
+              val hex = json.substring(pos + 1, pos + 5)
+              sb.append(Integer.parseInt(hex, 16).toChar)
+              pos += 4
             case c => sb.append(c)
           }
         } else {
@@ -78,8 +95,7 @@ object ViashJsonParser {
         }
         pos += 1
       }
-      if (pos >= json.length) throw new RuntimeException("Unterminated string")
-      pos += 1
+      consume('"')
       sb.toString
     }
     
@@ -88,114 +104,89 @@ object ViashJsonParser {
       if (json(pos) == '-') pos += 1
       while (pos < json.length && json(pos).isDigit) pos += 1
       
-      var isDouble = false
-      if (pos < json.length && json(pos) == '.') {
-        isDouble = true
+      val hasDecimal = pos < json.length && json(pos) == '.'
+      if (hasDecimal) {
         pos += 1
         while (pos < json.length && json(pos).isDigit) pos += 1
       }
       
-      if (pos < json.length && (json(pos) == 'e' || json(pos) == 'E')) {
-        isDouble = true
+      val hasExponent = pos < json.length && (json(pos) == 'e' || json(pos) == 'E')
+      if (hasExponent) {
         pos += 1
         if (pos < json.length && (json(pos) == '+' || json(pos) == '-')) pos += 1
         while (pos < json.length && json(pos).isDigit) pos += 1
       }
       
       val numStr = json.substring(start, pos)
-      if (isDouble) numStr.toDouble else numStr.toInt
+      if (hasDecimal || hasExponent) {
+        numStr.toDouble
+      } else {
+        val n = BigInt(numStr)
+        if (n.isValidInt) n.toInt
+        else if (n.isValidLong) n.toLong
+        else n.toDouble
+      }
     }
     
     def parseBoolean(): Boolean = {
-      if (pos + 4 <= json.length && json.substring(pos, pos + 4) == "true") {
+      if (json.substring(pos).startsWith("true")) {
         pos += 4
         true
-      } else if (pos + 5 <= json.length && json.substring(pos, pos + 5) == "false") {
+      } else if (json.substring(pos).startsWith("false")) {
         pos += 5
         false
       } else {
-        throw new RuntimeException("Invalid boolean")
+        throw new RuntimeException(s"Invalid boolean at position $pos")
       }
     }
     
     def parseNull(): Null = {
-      if (pos + 4 <= json.length && json.substring(pos, pos + 4) == "null") {
+      if (json.substring(pos).startsWith("null")) {
         pos += 4
         null
       } else {
-        throw new RuntimeException("Invalid null")
+        throw new RuntimeException(s"Invalid null at position $pos")
       }
     }
     
     def parseArray(): List[Any] = {
-      pos += 1
-      skipWhitespace()
-      
-      if (pos < json.length && json(pos) == ']') {
-        pos += 1
-        return List.empty
+      consume('[')
+      if (peek == ']') {
+        consume(']')
+        return Nil
       }
       
-      val result = scala.collection.mutable.ListBuffer[Any]()
-      while (true) {
-        result += parseValue()
-        skipWhitespace()
-        
-        if (pos >= json.length) throw new RuntimeException("Unterminated array")
-        
-        if (json(pos) == ']') {
-          pos += 1
-          return result.toList
-        } else if (json(pos) == ',') {
-          pos += 1
-          skipWhitespace()
-        } else {
-          throw new RuntimeException(s"Expected ',' or ']'")
-        }
+      val items = scala.collection.mutable.ListBuffer[Any]()
+      items += parseValue()
+      while (peek == ',') {
+        consume(',')
+        items += parseValue()
       }
-      List.empty
+      consume(']')
+      items.toList
     }
     
     def parseObject(): Map[String, Any] = {
-      pos += 1
-      skipWhitespace()
-      
-      if (pos < json.length && json(pos) == '}') {
-        pos += 1
+      consume('{')
+      if (peek == '}') {
+        consume('}')
         return Map.empty
       }
       
-      val result = scala.collection.mutable.Map[String, Any]()
-      while (true) {
-        skipWhitespace()
-        
-        if (pos >= json.length || json(pos) != '"') {
-          throw new RuntimeException("Expected string key")
-        }
+      val entries = scala.collection.mutable.Map[String, Any]()
+      def parseEntry(): Unit = {
         val key = parseString()
-        
-        skipWhitespace()
-        if (pos >= json.length || json(pos) != ':') {
-          throw new RuntimeException("Expected ':'")
-        }
-        pos += 1
-        
-        val value = parseValue()
-        result(key) = value
-        
-        skipWhitespace()
-        if (pos >= json.length) throw new RuntimeException("Unterminated object")
-        
-        if (json(pos) == '}') {
-          pos += 1
-          return result.toMap
-        } else if (json(pos) == ',') {
-          pos += 1
-        } else {
-          throw new RuntimeException(s"Expected ',' or '}'")
-        }
+        consume(':')
+        entries(key) = parseValue()
       }
-      Map.empty
+      
+      parseEntry()
+      while (peek == ',') {
+        consume(',')
+        parseEntry()
+      }
+      consume('}')
+      entries.toMap
     }
     
     parseValue()
--- src/main/scala/io/viash/languages/Scala.scala
+++ src/main/scala/io/viash/languages/Scala.scala
@@ -18,7 +18,7 @@
 package io.viash.languages
 
 import io.viash.helpers.Resources
-import io.viash.config.arguments.Argument
+import io.viash.config.arguments._
 import io.viash.config.Config
 import io.viash.config.resources.ScriptInjectionMods
 
@@ -38,13 +38,104 @@ object Scala extends Language {
       .mkString("\n")
     
     val paramsCode = if (argsMetaAndDeps.nonEmpty) {
-      // Parse JSON once and extract all sections
-      val parseOnce = "// Parse JSON parameters once and extract all sections\nval _viashJsonData = ViashJsonParser.parseJson()\n"
-      val extractSections = argsMetaAndDeps.map { case (dest, _) =>
-        s"val $dest = _viashJsonData.getOrElse(\"$dest\", Map.empty[String, Any])"
-      }.mkString("\n")
+      // Parse JSON once
+      val parseOnce = "// Parse JSON parameters\nval _viashJsonData = ViashJsonParser.parseJson()\n\n"
       
-      parseOnce + extractSections
+      // Generate case class and instance for each section (par, meta, dep)
+      val sections = argsMetaAndDeps.map { case (dest, params) =>
+        val className = s"Viash${dest.capitalize}"
+        
+        // Generate case class field types
+        val classTypes = params.map { par =>
+          val classType = par match {
+            case a: BooleanArgumentBase if a.multiple => "List[Boolean]"
+            case a: IntegerArgument if a.multiple => "List[Int]"
+            case a: LongArgument if a.multiple => "List[Long]"
+            case a: DoubleArgument if a.multiple => "List[Double]"
+            case a: FileArgument if a.multiple => "List[String]"
+            case a: StringArgument if a.multiple => "List[String]"
+            // Optional types for non-required, non-flag arguments
+            case a: BooleanArgumentBase if !a.required && a.flagValue.isEmpty => "Option[Boolean]"
+            case a: IntegerArgument if !a.required => "Option[Int]"
+            case a: LongArgument if !a.required => "Option[Long]"
+            case a: DoubleArgument if !a.required => "Option[Double]"
+            case a: FileArgument if !a.required => "Option[String]"
+            case a: StringArgument if !a.required => "Option[String]"
+            // Required types
+            case _: BooleanArgumentBase => "Boolean"
+            case _: IntegerArgument => "Int"
+            case _: LongArgument => "Long"
+            case _: DoubleArgument => "Double"
+            case _: FileArgument => "String"
+            case _: StringArgument => "String"
+          }
+          s"  ${par.plainName}: $classType"
+        }
+        
+        // Generate JSON extraction code for each parameter
+        val extractors = params.map { par =>
+          val jsonKey = par.plainName
+          val extractor = par match {
+            // Multiple values - extract as List (handle null as empty list)
+            case a: BooleanArgumentBase if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(_.toString.toBoolean)).getOrElse(Nil)"""
+            case a: IntegerArgument if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(v => v match { case i: Int => i; case d: Double => d.toInt; case s => s.toString.toInt })).getOrElse(Nil)"""
+            case a: LongArgument if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(v => v match { case i: Int => i.toLong; case l: Long => l; case d: Double => d.toLong; case s => s.toString.toLong })).getOrElse(Nil)"""
+            case a: DoubleArgument if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(v => v match { case d: Double => d; case i: Int => i.toDouble; case s => s.toString.toDouble })).getOrElse(Nil)"""
+            case a: FileArgument if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(_.toString)).getOrElse(Nil)"""
+            case a: StringArgument if a.multiple =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.asInstanceOf[List[Any]].map(_.toString)).getOrElse(Nil)"""
+            
+            // Optional values
+            case a: BooleanArgumentBase if !a.required && a.flagValue.isEmpty =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString.toBoolean)"""
+            case a: IntegerArgument if !a.required =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case i: Int => i; case d: Double => d.toInt; case s => s.toString.toInt })"""
+            case a: LongArgument if !a.required =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case i: Int => i.toLong; case l: Long => l; case d: Double => d.toLong; case s => s.toString.toLong })"""
+            case a: DoubleArgument if !a.required =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case d: Double => d; case i: Int => i.toDouble; case s => s.toString.toDouble })"""
+            case a: FileArgument if !a.required =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString)"""
+            case a: StringArgument if !a.required =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString)"""
+            
+            // Required values (handle null as default value)
+            case _: BooleanArgumentBase =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString.toBoolean).getOrElse(false)"""
+            case _: IntegerArgument =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case i: Int => i; case d: Double => d.toInt; case s => s.toString.toInt }).getOrElse(0)"""
+            case _: LongArgument =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case i: Int => i.toLong; case l: Long => l; case d: Double => d.toLong; case s => s.toString.toLong }).getOrElse(0L)"""
+            case _: DoubleArgument =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(v => v match { case d: Double => d; case i: Int => i.toDouble; case s => s.toString.toDouble }).getOrElse(0.0)"""
+            case _: FileArgument =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString).getOrElse("")"""
+            case _: StringArgument =>
+              s"""_${dest}Json.get("$jsonKey").flatMap(v => Option(v)).map(_.toString).getOrElse("")"""
+          }
+          s"  $extractor"
+        }
+        
+        // Generate the case class definition
+        val caseClassDef = s"""case class $className(
+${classTypes.mkString(",\n")}
+)"""
+        
+        // Generate the JSON extraction and instance creation
+        val extraction = s"""val _${dest}Json = _viashJsonData.getOrElse("$dest", Map.empty[String, Any]).asInstanceOf[Map[String, Any]]
+val $dest = $className(
+${extractors.mkString(",\n")}
+)"""
+        
+        caseClassDef + "\n" + extraction
+      }
+      
+      parseOnce + sections.mkString("\n\n")
     } else {
       ""
     }
