4106a7d2516d70ddadeedc22063d8c11d0e2589d
Robrecht Cannoodt <rcannood@gmail.com>
2026-02-06 10:48:43 +0100

    refactor bash util test suite

--- src/test/scala/io/viash/helpers/BashUtilsTest.scala
+++ src/test/scala/io/viash/helpers/BashUtilsTest.scala
@@ -14,25 +14,35 @@ class BashUtilsTest extends AnyFunSuite with BeforeAndAfterAll {
   private val projectRoot = Paths.get(System.getProperty("user.dir"))
   private val bashUtilsTestDir = projectRoot.resolve("src/test/resources/io/viash/helpers/bashutils")
 
-  test("ViashParseArgumentValue") {
-    val testScript = bashUtilsTestDir.resolve("ViashParseArgumentValue.test.sh")
+  /** Helper method to run a bash test script and assert it passes */
+  private def runBashTest(scriptName: String, testName: String): Unit = {
+    val testScript = bashUtilsTestDir.resolve(scriptName)
     
     val result = Exec.runCatchPath(
       List("bash", testScript.toString),
       cwd = Some(projectRoot)
     )
     
-    assert(result.exitValue == 0, s"ViashParseArgumentValue test failed:\n${result.output}")
+    assert(result.exitValue == 0, s"$testName test failed:\n${result.output}")
   }
 
-  test("ViashCleanupRegistry") {
-    val testScript = bashUtilsTestDir.resolve("ViashCleanupRegistry.test.sh")
-    
-    val result = Exec.runCatchPath(
-      List("bash", testScript.toString),
-      cwd = Some(projectRoot)
-    )
-    
-    assert(result.exitValue == 0, s"ViashCleanupRegistry test failed:\n${result.output}")
+  // Define test cases as (test name, script filename)
+  private val testCases = Seq(
+    ("ViashParseArgumentValue", "ViashParseArgumentValue.test.sh"),
+    ("ViashCleanupRegistry", "ViashCleanupRegistry.test.sh"),
+    ("ViashLogging", "ViashLogging.test.sh"),
+    ("ViashQuote", "ViashQuote.test.sh"),
+    ("ViashRenderJson", "ViashRenderJson.test.sh"),
+    ("ViashAbsolutePath", "ViashAbsolutePath.test.sh"),
+    ("ViashRemoveFlags", "ViashRemoveFlags.test.sh"),
+    ("ViashSourceDir and ViashFindTargetDir", "ViashSourceDir.test.sh"),
+    ("ViashDockerAutodetectMount", "ViashDockerAutodetectMount.test.sh")
+  )
+
+  // Register all tests
+  testCases.foreach { case (testName, scriptName) =>
+    test(testName) {
+      runBashTest(scriptName, testName)
+    }
   }
 }
