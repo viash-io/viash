f5cbc522c5e8f63fb4deb44f7aed263d505e8042
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2026-09-08 11:35:47 +0200

    fix escaping of filenames in a regex

--- src/main/scala/io/viash/config/ConfigMeta.scala
+++ src/main/scala/io/viash/config/ConfigMeta.scala
@@ -19,6 +19,7 @@ package io.viash.config
 
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.regex.Pattern
 import io.circe.yaml.Printer
 
 import io.viash.config.resources.PlainFile
@@ -117,7 +118,7 @@ object ConfigMeta {
     // replace text placeholders with nice multiline string
     val configYamlStr2 = placeholderMap.foldLeft(configYamlStr) {
       case (configStr, (res, placeholder)) =>
-        val IndentRegex = ("( *)text: \"" + placeholder + "\"").r
+        val IndentRegex = ("( *)text: \"" + Pattern.quote(placeholder) + "\"").r
         val indent = IndentRegex.findFirstMatchIn(configStr)
           .map(_.group(1))
           .getOrElse(throw new RuntimeException(s"Could not find placeholder for text field in generated yaml: $placeholder"))
