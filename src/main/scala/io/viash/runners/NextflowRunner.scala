631cbd4fd1fca48bb913e8361d0f6b11ea7f978e
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2026-09-15 15:15:04 +0200

    fix pipes in nextflow handling

--- src/main/scala/io/viash/runners/NextflowRunner.scala
+++ src/main/scala/io/viash/runners/NextflowRunner.scala
@@ -225,15 +225,15 @@ final case class NextflowRunner(
           |  return ${scr.entrypoint}
           |}""".stripMargin
       // else if it is a vdsl3 module
-      case _ => 
+      case _ =>
         s"""// inner workflow hook
           |def innerWorkflowFactory(args) {
-          |  def rawScript = ${NextflowHelper.generateScriptStr(config)}
-          |  
+          |  def rawScript = ${NextflowHelper.generateScriptStr(config).split("\n").mkString("\n|")}
+          |
           |  return vdsl3WorkflowFactory(args, meta, rawScript)
           |}
           |
-          |""".stripMargin + 
+          |""".stripMargin +
           NextflowHelper.vdsl3Helper
     }
 
@@ -248,7 +248,7 @@ final case class NextflowRunner(
       |// create meta object
       |meta = [
       |  "resources_dir": moduleDir.toRealPath().normalize(),
-      |  "config": ${NextflowHelper.generateConfigStr(config)}
+      |  "config": ${NextflowHelper.generateConfigStr(config).split("\n").mkString("\n|")}
       |]
       |
       |// resolve dependencies dependencies (if any)
@@ -258,7 +258,7 @@ final case class NextflowRunner(
       |${innerWorkflowFactory.split("\n").mkString("\n|")}
       |
       |// defaults
-      |meta["defaults"] = ${NextflowHelper.generateDefaultWorkflowArgs(config, directivesToJson, auto, debug)}
+      |meta["defaults"] = ${NextflowHelper.generateDefaultWorkflowArgs(config, directivesToJson, auto, debug).split("\n").mkString("\n|")}
       |
       |// initialise default workflow
       |meta["workflow"] = workflowFactory([key: meta.config.name], meta.defaults, meta)
